c7704ad123df5b352fcafab59016835743180a17
Michael T. Gastner <mgastner@gmail.com>
2020-07-22 23:40:36 +0800

    Example with CGAL polygon

--- cartogram_generator/main.cpp
+++ cartogram_generator/main.cpp
@@ -1,6 +1,8 @@
 #include <mapbox/geojson.hpp>
 #include <mapbox/geojson/rapidjson.hpp>
 #include <mapbox/geometry.hpp>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Polygon_2.h>
 
 #include <iostream>
 #include <fstream>
@@ -8,6 +10,10 @@
 
 using namespace std;
 using namespace mapbox::geojson;
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef K::Point_2 Point;
+typedef CGAL::Polygon_2<K> Polygon_2;
 
 template <typename T = geojson>
 geojson readGeoJSON(const string &path, bool use_convert) {
@@ -26,4 +32,15 @@ int main() {
   bool use_convert = true;
   const auto data = readGeoJSON("../sample_data/feature-collection.json",
                                 use_convert);
+  //const auto &features = data.get<feature_collection>();
+  Point points[] = { Point(0,0), Point(5.1,0), Point(1,1), Point(0.5,6)};
+  Polygon_2 pgn(points, points+4);
+  // check if the polygon is simple.
+  cout << "The polygon is " <<
+    (pgn.is_simple() ? "" : "not ") << "simple." << endl;
+  // check if the polygon is convex
+  cout << "The polygon is " <<
+    (pgn.is_convex() ? "" : "not ") << "convex." << endl;
+
+  return 0;
 }
