ce1043ac37e99cfbcca5ed8d1e67be8e3b360aec
mgastner <mgastner@gmail.com>
2020-09-22 10:44:06 +0800

    Insert function check_geojson_validity()

--- README.md
+++ README.md
@@ -3,16 +3,9 @@ Cartogram generator in C++
 
 On Ubuntu, I had to do this:
 
-
-
-    Enable the repository:
-
-    # sudo add-apt-repository ppa:team-xbmc/ppa
-
-    Update the package index:
-
-    # sudo apt-get update
-
-    Install nlohmann-json3-dev deb package:
-
-    # sudo apt-get install nlohmann-json3-dev
+- Go to https://github.com/nlohmann/json
+- Click on "Code" -> "Download Zip"
+- Go to Downloads folder
+- cmake .
+- make
+- sudo make install
--- cartogram_generator/read_geojson.cpp
+++ cartogram_generator/read_geojson.cpp
@@ -1,9 +1,69 @@
+// TODO:
+// - IMPROVE ERROR HANDLING
+// - ADD SUPPORT FOR "geometry": "Polygon"
+
 #include <nlohmann/json.hpp>
 #include <iostream>
 #include <fstream>
 
+using json = nlohmann::json;
+
+void check_geojson_validity(json j)
+{
+  if (!j.contains(std::string{"type"})) {
+    std::cerr << "ERROR: JSON does not contain a key 'type'" << std::endl;
+    _Exit(4);
+  }
+  if (j["type"] != "FeatureCollection") {
+    std::cerr << "ERROR: JSON is not a valid GeoJSON FeatureCollection"
+              << std::endl;
+    _Exit(5);
+  }
+  if (!j.contains(std::string{"features"})) {
+    std::cerr << "ERROR: JSON does not contain a key 'features'" << std::endl;
+    _Exit(6);
+  }
+  json features = j["features"];
+  for (auto feature : features) {
+    if (!feature.contains(std::string{"type"})) {
+      std::cerr << "ERROR: JSON contains a 'Features' element without key "
+                << "'type'" << std::endl;
+      _Exit(7);
+    }
+    if (feature["type"] != "Feature") {
+      std::cerr << "ERROR: JSON contains a 'Features' element whose type "
+                << "is not 'Feature'" << std::endl;
+      _Exit(8);
+    }
+    if (!feature.contains(std::string("geometry"))) {
+      std::cerr << "ERROR: JSON contains a feature without key 'geometry'"
+                << std::endl;
+      _Exit(9);
+    }
+    json geometry = feature["geometry"];
+    if (!geometry.contains(std::string("type"))) {
+      std::cerr << "ERROR: JSON contains geometry without key 'type'"
+                << std::endl;
+      _Exit(10);
+    }
+    if (!geometry.contains(std::string("coordinates"))) {
+      std::cerr << "ERROR: JSON contains geometry without key 'coordinates'"
+                << std::endl;
+      _Exit(11);
+    }
+    if (geometry["type"] != "MultiPolygon" && geometry["type"] != "Polygon") {
+      std::cerr << "ERROR: JSON contains unsupported geometry "
+                << geometry["type"] << std::endl;
+      _Exit(12);
+    }
+  }
+
+  return;
+}
+
 void read_geojson(std::string geometry_file_name)
 {
+  // Open file.
   std::ifstream in_file(geometry_file_name);
   if (!in_file) {
     throw std::system_error(errno,
@@ -11,7 +71,7 @@ void read_geojson(std::string geometry_file_name)
                             "failed to open " + geometry_file_name);
   }
 
-  using json = nlohmann::json;
+  // Parse JSON.
   json j;
   try {
     in_file >> j;
@@ -21,6 +81,15 @@ void read_geojson(std::string geometry_file_name)
               << "byte position of error: " << e.byte << std::endl;
     _Exit(3);
   }
-
+  check_geojson_validity(j);
+  json features = j["features"];
+  for (auto feature : features) {
+    json geometry = feature["geometry"];
+    if (geometry["type"] == "Polygon") {
+      std::cerr << "ERROR: Sorry, no support for Polygon geometry yet"
+                << std::endl;
+      _Exit(13);
+    }
+  }
   return;
 }
