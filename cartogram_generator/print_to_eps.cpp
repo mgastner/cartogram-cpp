261ec03ae371ba343e597247450751a763a7c64c
mgastner <mgastner@gmail.com>
2020-10-16 23:02:11 +0800

    Print exterior rings

--- cartogram_generator/print_to_eps.cpp
+++ cartogram_generator/print_to_eps.cpp
@@ -35,8 +35,33 @@ void print_to_eps(std::string eps_name, MapState *map_state)
   eps_file << "/sgry {setgray} def\n";
   eps_file << "/srgb {setrgbcolor} def\n";
 
-  eps_file << "100 100 moveto 350 0 rlineto 0 350 rlineto -350 0 rlineto "
-           << "closepath stroke\n";
+  // Print polygons
+  for (auto gd : map_state->get_geo_divs()) {
+    for (auto pwh : gd.get_polygons_with_holes()) {
+      CGAL::Polygon_2<K> pgn = pwh.outer_boundary();
+
+      // Move to starting coordinates
+      eps_file << "n " << pgn[0][0] << " " << pgn[0][1] << " m\n";
+
+      // Plot each point in exterior ring
+      for (int i = 1; i < pgn.size(); ++i) {
+        eps_file << pgn[i][0] << " " << pgn[i][1] << " l\n";
+      }
+
+      // Close path
+      eps_file << "c\n";
+
+      // Save path before filling it
+      eps_file << "gsave\n";
+
+      // Fill path
+      eps_file << "0.96 0.92 0.70 srgb f\n";
+
+      // Restore path and stroke outline
+      eps_file << "grestore\n";
+      eps_file << "0 sgry s\n";
+    }
+  }
   eps_file << "showpage\n";
   eps_file << "%%EOF\n";
   eps_file.close();
