7044a87178293b6d36c65e353a13dc849080c634
mgastner <mgastner@gmail.com>
2020-10-16 22:28:21 +0800

    \n instead of std::endl to reduce flushing buffer

--- cartogram_generator/print_to_eps.cpp
+++ cartogram_generator/print_to_eps.cpp
@@ -11,25 +11,34 @@ void print_to_eps(std::string eps_name, MapState *map_state)
   struct tm *ti = localtime(&tt);
 
   // EPS header
-  eps_file << "%!PS-Adobe-3.0 EPSF-3.0 " << std::endl;
-  eps_file << "%!PS-Adobe-2.0 EPSF-2.0" << std::endl;
-  eps_file << "%%Title: " << eps_name << std::endl;
-  eps_file << "%%Creator: Michael T. Gastner et al." << std::endl;
-  eps_file << "%%For: Humanity" << std::endl;
-  eps_file << "%%Copyright: License CC BY-NC-ND 2.0" << std::endl;
+  eps_file << "%!PS-Adobe-3.0 EPSF-3.0\n";
+  eps_file << "%%Title: " << eps_name << "\n";
+  eps_file << "%%Creator: Michael T. Gastner et al.\n";
+  eps_file << "%%For: Humanity\n";
+  eps_file << "%%Copyright: License CC BY-NC-ND 2.0\n";
   eps_file << "%%CreationDate: " << asctime(ti);
-  eps_file << "%%Pages: 1" << std::endl;
+  eps_file << "%%Pages: 1\n";
   eps_file << "%%BoundingBox: 0 0 "
            << map_state->get_lx() << " "
-           << map_state->get_ly() << std::endl;
-  eps_file << "%%Magnification: 1.0000" << std::endl;
-  eps_file << "%%EndComments" << std::endl;
+           << map_state->get_ly() << "\n";
+  eps_file << "%%Magnification: 1.0000\n";
+  eps_file << "%%EndComments\n";
+
+  // Definitions
+  eps_file << "/m {moveto} def\n";
+  eps_file << "/l {lineto} def\n";
+  eps_file << "/s {stroke} def\n";
+  eps_file << "/n {newpath} def\n";
+  eps_file << "/c {closepath} def\n";
+  eps_file << "/f {fill} def\n";
+  eps_file << "/slw {setlinewidth} def\n";
+  eps_file << "/sgry {setgray} def\n";
+  eps_file << "/srgb {setrgbcolor} def\n";
 
   eps_file << "100 100 moveto 350 0 rlineto 0 350 rlineto -350 0 rlineto "
-           << "closepath stroke" << std::endl;
-
-  eps_file << "showpage" << std::endl;
-  eps_file << "%%EOF" << std::endl;
+           << "closepath stroke\n";
+  eps_file << "showpage\n";
+  eps_file << "%%EOF\n";
   eps_file.close();
   return;
 }
