a506ffb8687294097c4b1c4badbb18291f34ffd4
mgastner <mgastner@gmail.com>
2020-12-29 11:17:53 +0800

    Declare 'eul'

--- cartogram_generator/flatten_density.cpp
+++ cartogram_generator/flatten_density.cpp
@@ -11,8 +11,8 @@ void flatten_density(MapState *map_state)
   const unsigned int ly = map_state->ly();
 
   // Allocate memory for the velocity grid
-  boost::multi_array<double, 2>  grid_vx(boost::extents[lx][ly]);
-  boost::multi_array<double, 2>  grid_vy(boost::extents[lx][ly]);
+  boost::multi_array<double, 2> grid_vx(boost::extents[lx][ly]);
+  boost::multi_array<double, 2> grid_vy(boost::extents[lx][ly]);
 
   // Prepare Fourier transforms for the flux
   FTReal2d grid_fluxx_init(lx, ly);
@@ -25,6 +25,11 @@ void flatten_density(MapState *map_state)
     fftw_plan_r2r_2d(lx, ly,
                      grid_fluxy_init.array(), grid_fluxy_init.array(),
                      FFTW_REDFT01, FFTW_RODFT01, FFTW_ESTIMATE);
+
+  // eul[i][j] will be the new position of proj[i][j] proposed by a simple
+  // Euler step: move a full time interval delta_t with the velocity at time t
+  // and position (proj[i][j].x, proj[i][j].y)
+  boost::multi_array<Point, 2> eul(boost::extents[lx][ly]);
 
   fftw_destroy_plan(plan_for_grid_fluxx_init);
   fftw_destroy_plan(plan_for_grid_fluxy_init);
