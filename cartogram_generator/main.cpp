2d689a421b568c926109a4c7cdedf13b15155a72
adisidev <64905594+adisidev@users.noreply.github.com>
2021-03-18 00:05:55 +0800

    Commented Densify code for bug-fixing

--- cartogram_generator/main.cpp
+++ cartogram_generator/main.cpp
@@ -170,7 +170,7 @@ int main(const int argc, const char *argv[])
     }
     flatten_density(&map_state);
     //project(&map_state);
-    map_state.set_geo_divs(densify(map_state.geo_divs()));
+    // map_state.set_geo_divs(densify(map_state.geo_divs()));
     project_graticule_centroids(&map_state);
     project_with_triangulation(&map_state);
     map_state.inc_integration();
