96601588fa580d9d913be9329f1bf43caac0db2f
adisidev <64905594+adisidev@users.noreply.github.com>
2021-06-03 21:00:06 +0530

    Revert "Revert "Merged new version of fill with density""
    This reverts commit 0b51dd69e22e7196ec5a5071fdd9f1cb767fcec1.

--- cartogram_generator/map_state.h
+++ cartogram_generator/map_state.h
@@ -18,6 +18,7 @@ private:
   std::string inset_pos_;
   std::vector<GeoDiv> geo_divs_;
   std::map<std::string, double> target_areas;
+  std::map<std::string, double> area_errs;
   std::map<std::string, Color> colors;
   unsigned int lx_, ly_;  // Lattice dimensions
   unsigned int new_xmin_, new_ymin_; // To store map translation vector
@@ -58,7 +59,9 @@ public:
   unsigned int n_finished_integrations() const;
   void inc_integration();
   boost::multi_array<XYPoint, 2> *proj();
-  double max_area_err();
+  void set_area_errs();
+  double area_errs_at(const std::string) const;
+  double max_area_err() const;
   void set_inset_pos(std::string inset_pos);
   const std::string inset_pos() const;
 };
