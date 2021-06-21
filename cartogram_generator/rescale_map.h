491d6ee57c04775a28f4ac7db88f71b474fe03d9
mgastner <mgastner@gmail.com>
2021-06-21 15:40:21 +0800

    Make long_grid_side_length unsigned

--- cartogram_generator/rescale_map.h
+++ cartogram_generator/rescale_map.h
@@ -1,7 +1,7 @@
 #ifndef RESCALE_MAP_H_
 #define RESCALE_MAP_H_
 
-void rescale_map(int, InsetState*, bool);
+void rescale_map(unsigned int, InsetState*, bool);
 void unscale_map(InsetState*);
 
 #endif
