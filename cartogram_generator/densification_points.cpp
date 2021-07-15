010c0f848968f733ff092bea2d2733df59a46efe
Phong Le <phongnguyen.le22@gmail.com>
2021-07-15 15:44:41 +0800

    Clean up densify code, add round_digits constant

--- cartogram_generator/densification_points.cpp
+++ cartogram_generator/densification_points.cpp
@@ -1,5 +1,5 @@
 #include "inset_state.h"
-// #include "cgal_typedef.h"
+#include "constants.h"
 #include <CGAL/intersections.h>
 
 // Returns ceiling up to nearest 0.5 value, e.g. 2.64 returns 3.5
@@ -20,64 +20,35 @@ double half_ceil(double num) {
 
   // Else flooring
   return floor(num) + 0.5;
-
-  //return floor(num + 0.5) + 0.5;
-
-}
-
-double simple_half_ceil(double num){
-  return floor(num + 0.5) + 0.5;
 }
 
 // Returns floor up to nearest 0.5 value, e.g. 2.23 returns 1.5
-double half_floor(double num) {
-
-  // Extracting decimal
-  double decimal = num - floor(num);
-
-  // Checking whether decimal is 0.5
-  if (abs(decimal - 0.5) < 1e-10) {
-    decimal = 0.5;
-  }
-
-  // Checking whether to add 0.5
-  if (decimal >= 0.5) {
-    return floor(num) + 0.5;
-  }
-
-  // Else subtracting 0.5
-  return floor(num) - 0.5;
-
-  //return floor(num + 0.5) - 0.5;
-}
-
-double simple_half_floor(double num){
+double half_floor(double num){
   return floor(num + 0.5) - 0.5;
 }
 
+// Very similar doubles
 bool almost_equal(double a, double b) {
-
-  // Very similar doubles
   return abs(a - b) <= 1e-10;
 }
 
+// Very similar points
 bool point_almost_equal(Point a, Point b) {
-
-  // Very similar points
   return (almost_equal(a[0], b[0]) && almost_equal(a[1], b[1]));
 }
 
+// Truncate a double down to round_digits digits after decimal point.
+// round_digits is defined in constants.h
 double round_down(double value) {
-    return floor(value * 1e9) / 1e9;
+  return floor(value * round_digits) / round_digits;
 }
 
+// Truncate the coordinates of a point
 Point round_point(Point p1) {
-
   double x1 = round_down(p1[0]);
   double y1 = round_down(p1[1]);
   Point rounded_point(x1, y1);
   return rounded_point;
-
 }
 
 // Returns intersection of line with vertical grid line
@@ -97,7 +68,6 @@ double calc_x_intersection(Point a, Point b, double y) {
 std::vector<Point> densification_points(Point a, Point b)
 {
 
-  // CGAL::set_pretty_mode(std::cout);
   // Vector to store all intersections
   std::vector<Point> intersections;
   intersections.push_back(a);
@@ -134,13 +104,6 @@ std::vector<Point> densification_points(Point a, Point b)
     }
   }
 
-  // std::cout << "Vertical intersections added!" << '\n';
-  // std::cout << "Current state: " << '\n';
-  // for (size_t i = 0; i < intersections.size(); i++) {
-  //   std::cout << i + 1 << ": ";
-  //   std::cout << intersections[i] << '\n';
-  // }
-
   // Horizontal intersections (intersections with y coordinate lines)
   if (a[1] < b[1]) {
 
@@ -150,24 +113,10 @@ std::vector<Point> densification_points(Point a, Point b)
         // Get x coordinate, y coordinate = y
         double x = calc_x_intersection(a, b, y);
         Point temp(x, y);
-        // double difference = 3.5 - x;
-        // printf("x with printf: %.10e\n", x);
-        // printf("difference with printf: %.10e\n", difference);
 
-        // Ensuring no corner points are pushed back
-        // As they would've already been added when calculating
-        // Vertical intersections
-        // if ((!almost_equal(x, half_floor(x)) && !almost_equal(y, half_floor(y))) || a[0] == b[0]) {
+        // Ensuring no corner points are pushed back as they would've already
+        // been added when calculating vertical intersections
         if (x != half_floor(x) || a[0] == b[0]) {
-
-          // std::cout << "x != half_floor(x) ?" << '\n';
-          // x != half_floor(x) ? std::cout << "true" : std::cout << "false";
-          // std::cout << '\n';
-          // std::cout << "x: " << std::setprecision(10) << x << '\n';
-          // std::cout << "half_floor(x): " << half_floor(x) << '\n';
-          // std::cout << "a[0] == b[0] ?" << '\n';
-          // a[0] == b[0] ? std::cout << "true" : std::cout << "false";
-          // std::cout << '\n';
           if (!point_almost_equal(a, temp) && !point_almost_equal(b, temp)) {
             intersections.push_back(round_point(temp));
           }
@@ -177,32 +126,12 @@ std::vector<Point> densification_points(Point a, Point b)
   } else if (a[1] > b[1]) {
 
     // y of "a" > y of "b"
-    // for (double y = half_floor(a[1]); y > b[1]; --y) {
-    //   if (!almost_equal(y, a[1]) && !almost_equal(y, b[1])) {
-
-    //     // Get x coordinate, y coordinate = y
-    //     double x = calc_x_intersection(a, b, y);
-    //     Point temp(x, y);
-    //     // Ensuring no corner points are pushed back
-    //     if ((!almost_equal(x, half_floor(x)) && !almost_equal(y, half_floor(y))) || a[0] == b[0]) {
-    //       if (!point_almost_equal(a, temp) && !point_almost_equal(b, temp)) {
-    //         intersections.push_back(round_point(temp));
-    //       }
-    //     }
-    //   }
-    // }
     for (double y = half_floor(a[1]); y > b[1]; --y) {
       if (!almost_equal(y, a[1]) && !almost_equal(y, b[1])) {
 
         // Get x coordinate, y coordinate = y
         double x = calc_x_intersection(a, b, y);
         Point temp(x, y);
-        // Ensuring no corner points are pushed back
-        // if ((!almost_equal(x, half_floor(x)) && !almost_equal(y, half_floor(y))) || a[0] == b[0]) {
-        //   if (!point_almost_equal(a, temp) && !point_almost_equal(b, temp)) {
-        //     intersections.push_back(round_point(temp));
-        //   }
-        // }
         if (!point_almost_equal(a, temp) && !point_almost_equal(b, temp)) {
           intersections.push_back(round_point(temp));
         }
@@ -210,72 +139,17 @@ std::vector<Point> densification_points(Point a, Point b)
     }
   }
 
-  // std::cout << "Horizontal intersections added!" << '\n';
-  // std::cout << "Current state: " << '\n';
-  // for (size_t i = 0; i < intersections.size(); i++) {
-  //   std::cout << i + 1 << ": ";
-  //   std::cout << intersections[i] << '\n';
-  // }
-
-  // std::cout << "Graticule intersections calculated; Now sorting..." << '\n';
-
   // Sorting intersections
   std::sort(intersections.begin(), intersections.end());
-
-  // std::cout << "State after sorting:" << '\n';
-  // for (size_t i = 0; i < intersections.size(); i++) {
-  //   std::cout << i + 1 << ": ";
-  //   std::cout << intersections[i] << '\n';
-  // }
 
   // Clearing duplicates
   intersections.erase(unique(intersections.begin(), intersections.end()),
   intersections.end());
 
-  // std::cout << "Element 2: " << intersections[1] << '\n';
-  // std::cout << "Element 3: " << intersections[2] << '\n';
-  // std::cout << "Element 3 == Element 2 ? ";
-  // intersections[1] == intersections[2] ? std::cout << "yes" : std::cout << "no";
-  // std::cout << '\n';
-
-  // std::cout << "State after clearing:" << '\n';
-  // for (size_t i = 0; i < intersections.size(); i++) {
-  //   std::cout << i + 1 << ": ";
-  //   std::cout << intersections[i] << '\n';
-  // }
-
-  // // actual size
-  // std::set<Point> counter(intersections.begin(), intersections.end());
-  // intersections.assign(intersections.begin(), intersections.end());
-  //
-  // // for (int i = 0; i < intersections.size(); i++)
-  // //       counter.insert(intersections[i]);
-  // // double actual_size = counter.size();
-  //
-  // double actual_size = counter.size();
-  //
-  // std::cout << "Actual size: " << actual_size << '\n';
-  // std::cout << "Intersections size: " << intersections.size() << '\n';
-  //
-  // while (actual_size < intersections.size()) {
-  //   std::cout << "Does this even run?" << '\n';
-  //   intersections.erase(unique(intersections.begin(), intersections.end()),
-  //   intersections.end());
-  // }
-  //
-  //
-  // std::cout << "Duplicates cleared!" << '\n';
-  // std::cout << "Current state: " << '\n';
-  // for (size_t i = 0; i < intersections.size(); i++) {
-  //   std::cout << i + 1 << ": ";
-  //   std::cout << intersections[i] << '\n';
-  // }
-
   // Storing current size
   double size = intersections.size();
 
   // TODO: Fix bug for diaganols when 0 intersections
-  // TODO: Round to 10 decimal places
 
   // Inserting intersection with diagonals
 
@@ -293,12 +167,8 @@ std::vector<Point> densification_points(Point a, Point b)
                    bottom_right[1] + 1);
 
     // Finding intersection
-
-    // std::cout << "Calculating segments..." << '\n';
-
     Segment seg_diaganol(top_left, bottom_right);
     Segment seg_intersec(a, b);
-
     auto result = CGAL::intersection(seg_diaganol, seg_intersec);
     if (result) {
       if (const Point *s = boost::get<Point>(&*result)) {
@@ -308,13 +178,6 @@ std::vector<Point> densification_points(Point a, Point b)
       }
     }
   }
-
-  // std::cout << "bottom_right-top_left intersections added!" << '\n';
-  // std::cout << "Current state: " << '\n';
-  // for (size_t i = 0; i < intersections.size(); i++) {
-  //   std::cout << i + 1 << ": ";
-  //   std::cout << intersections[i] << '\n';
-  // }
 
   // Adding intersections with bottom_left-top_right diaganol
   for (unsigned int i = 0; i < size - 1; ++i) {
@@ -326,18 +189,6 @@ std::vector<Point> densification_points(Point a, Point b)
     // Top-right corner
     Point top_right(bottom_left[0] + 1,
                     bottom_left[1] + 1);
-
-    // if (a[0] >= 197.485 && a[0] <= 197.486 && a[1] >= 257.620 && a[1] <= 257.621 &&
-    //     b[0] >= 197.667 && b[0] <= 197.668 && b[1] >= 257.403 && b[1] <= 257.404) {
-    //
-    //   std::cout << "\nCurrent diagonal: \n"
-    //             << "(" << bottom_left[0] << ", " << bottom_left[1] << ")\n"
-    //             << "(" << top_right[0] << ", " << top_right[1] << ")\n";
-    //
-    //   std::cout << "Rounded from: \n"
-    //             << "(" << intersections[i][0] << ", " << intersections[i][1] << ")\n"
-    //             << "(" << intersections[i + 1][0] << ", " << intersections[i + 1][1] << ")\n";
-    // }
 
     // Finding intersection
     Segment seg_diaganol(top_right, bottom_left);
@@ -359,116 +210,7 @@ std::vector<Point> densification_points(Point a, Point b)
     }
   }
 
-  // std::cout << "bottom_left-top_right intersections added!" << '\n';
-  // std::cout << "Current state: " << '\n';
-  // for (size_t i = 0; i < intersections.size(); i++) {
-  //   std::cout << i + 1 << ": ";
-  //   std::cout << intersections[i] << '\n';
-  // }
-
-  // if (size == 0) {
-  //
-  //   std::cout << "if size 0, intersections added!" << '\n';
-  //   // Adding intersection with singular grid cell diaganols
-  //
-  //   // Bottom-right corner
-  //   Point bottom_right(half_floor(a[0]) + 1,
-  //                      half_floor(a[1]));
-  //
-  //   // Top-left corner
-  //   Point top_left(half_ceil(a[0]) - 1,
-  //                  half_ceil(a[1]));
-  //
-  //   // Bottom-left corner
-  //   Point bottom_left(half_floor(a[0]),
-  //                     half_floor(a[1]));
-  //
-  //   // Top-right corner
-  //   Point top_right(half_ceil(a[0]),
-  //                   half_ceil(a[1]));
-  //
-  //   // Finding intersections
-  //   Segment seg_tlbr(top_left, bottom_right);
-  //   Segment seg_trbl(top_right, bottom_left);
-  //   Segment seg_intersec(a, b);
-  //
-  //   auto result_1 = intersection(seg_tlbr, seg_intersec);
-  //   if (result_1) {
-  //     if (const Point *s = boost::get<Point>(&*result_1)) {
-  //       intersections.push_back(round_point(*s));
-  //     }
-  //   }
-  //
-  //   auto result_2 = intersection(seg_trbl, seg_intersec);
-  //   if (result_2) {
-  //     if (const Point *s = boost::get<Point>(&*result_2)) {
-  //       if (!((*s)[0] == floor((*s)[0]) || (*s)[1] == floor((*s)[1]))) {
-  //         intersections.push_back(round_point(*s));
-  //       }
-  //     }
-  //   }
-
-  // std::cout << "Current state: " << '\n';
-  // for (size_t i = 0; i < intersections.size(); i++) {
-  //   std::cout << i + 1 << ": ";
-  //   std::cout << intersections[i] << '\n';
-  // }
-  //
-  // } else {
-  //
-  //   std::cout << "size > 0, final intersection added!" << '\n';
-  //
-  //   // Adding final intersection using last intersection point
-  //
-  //   // Bottom-right corner
-  //   Point bottom_right(half_floor(a[0]) + 1,
-  //                      half_floor(a[1]));
-  //
-  //   // Top-left corner
-  //   Point top_left(half_ceil(a[0]) - 1,
-  //                  half_ceil(a[1]));
-  //
-  //   // Bottom-left corner
-  //   Point bottom_left(half_floor(a[0]),
-  //                     half_floor(a[1]));
-  //
-  //   // Top-right corner
-  //   Point top_right(half_ceil(a[0]),
-  //                   half_ceil(a[1]));
-  //
-  //   // Finding intersections
-  //   Segment seg_tlbr(top_left, bottom_right);
-  //   Segment seg_trbl(top_right, bottom_left);
-  //
-  //   Point c;
-  //   if (a[0] > b[0] || (a[1] > b[1] && a[0] == b[0])) {
-  //     c = a;
-  //   } else {
-  //     c = b;
-  //   }
-  //
-  //   Segment seg_intersec(intersections[size - 1], c);
-  //
-  //   auto result_1 = intersection(seg_tlbr, seg_intersec);
-  //   if (result_1) {
-  //     if (const Point *s = boost::get<Point>(&*result_1)) {
-  //       intersections.push_back(round_point(*s));
-  //     }
-  //   }
-  //
-  //   auto result_2 = intersection(seg_trbl, seg_intersec);
-  //   if (result_2) {
-  //     if (const Point *s = boost::get<Point>(&*result_2)) {
-  //       if (!((*s)[0] == floor((*s)[0]) || (*s)[1] == floor((*s)[1]))) {
-  //         intersections.push_back(round_point(*s));
-  //       }
-  //     }
-  //   }
-  // }
-
   // TODO: REMOVE DUPLICATES UNTIL NO MORE
-
-  // std::cout << "Diaganol intersections calculated! Now sorting..." << '\n';
 
   // Sorting intersections
   std::sort(intersections.begin(), intersections.end());
@@ -482,8 +224,6 @@ std::vector<Point> densification_points(Point a, Point b)
     // Sort in descending order of x coordinates
     std::reverse(intersections.begin(), intersections.end());
   }
-
-  // std::cout << "Sorted! Now returning..." << '\n';
-
+  
   return intersections;
 }
--- cartogram_generator/densify.cpp
+++ cartogram_generator/densify.cpp
@@ -19,23 +19,22 @@ std::vector<GeoDiv> densify(std::vector<GeoDiv> geodivs) {
   std::vector<GeoDiv> geodivs_dens;
   for (GeoDiv gd : geodivs) {
     GeoDiv gd_dens(gd.id());
-    std::cout << "Densifying: " << gd.id() << std::endl;
 
     for (Polygon_with_holes pgnwh : gd.polygons_with_holes()) {
       Polygon outer = pgnwh.outer_boundary();
       Polygon outer_dens;
       size_t outer_size = outer.size();
+
+      // Iterate over each point in the outer boundary of the GeoDiv
       for (size_t i = 0; i < outer_size; i++) {
+        
+        // The segment defined by points a and b is to be densified.
+        // b should be the point immediately after a, unless a is the final
+        // point of the boundary, in which case b should be the first point.
         Point a = outer[i];
-        Point b;
+        Point b = (i == outer_size - 1) ? outer[0] : outer[i + 1];
 
-        if (i == outer_size - 1){
-          b = outer[0];
-        } else {
-          b = outer[i + 1];
-        }
-
-        // std::cout << "Densifying outer pts \na: " << a << "\nb: " << b << "\ni: " << i  << "\nouter_size: " << outer_size << "\n\n";
+        // Densify the segment.
         std::vector<Point> outer_pts_dens = densification_points(a, b);
 
         if (abs(a[0] - b[0]) > 10 || abs(a[1] - b[1]) > 10) {
@@ -54,77 +53,72 @@ std::vector<GeoDiv> densify(std::vector<GeoDiv> geodivs) {
         //   }
         // }
 
-        // Pushing all points but not the last one
+        // Pushing all points except the last.
         for (size_t i = 0; i < (outer_pts_dens.size() - 1); i++) {
           outer_dens.push_back(outer_pts_dens[i]);
         }
-
       }
 
+      // Check for duplicate points in the densified outer boundary
       std::vector<Point> temp_out;
       for (size_t i = 0; i < outer_dens.size(); i++) {
         temp_out.push_back(outer_dens[i]);
       }
-
       if (duplicates(temp_out)) {
-        // for (size_t i = 0; i < temp_out.size(); i++) {
-          std::cout << "Duplicates found in outer boundary!" << std::endl;
-        // }
+        std::cout << "Duplicates found in outer boundary!" << std::endl;
       }
 
       std::vector<Polygon> holes_v_dens;
       std::vector<Polygon> holes_v(pgnwh.holes_begin(), pgnwh.holes_end());
+
+      // Iterate over each hole
       for (Polygon hole : holes_v) {
         Polygon hole_dens;
         for (size_t j = 0; j < hole.size(); j++) {
+          
+          // c and d are determined the same way as a and b above
           Point c = hole[j];
-          Point d;
-
-          if (j == hole.size() - 1){
-            d = hole[0];
-          } else {
-            d = hole[j + 1];
-          }
+          Point d = (j == hole.size() - 1) ? hole[0] : hole[j + 1];
 
           std::vector<Point> hole_pts_dens = densification_points(c, d);
 
-          if (duplicates(hole_pts_dens)) {
-            std::cout << "Original: c " << c << " d " << d << std::endl;
-            std::cout << "Densified: " << std::endl;
-            for (size_t i = 0; i < hole_pts_dens.size(); i++) {
-              std::cout << i + 1 << ": ";
-              std::cout << hole_pts_dens[i] << std::endl;
-            }
+          if (abs(c[0] - d[0]) > 10 || abs(c[1] - d[1]) > 10) {
+            std::cout << "Points VERY different!" << std::endl;
+            std::cout << "c: " << c << std::endl;
+            std::cout << "d: " << d << std::endl;
           }
+
+          // if (duplicates(hole_pts_dens)) {
+          //   std::cout << "Original: c " << c << " d " << d << std::endl;
+          //   std::cout << "Densified: " << std::endl;
+          //   for (size_t i = 0; i < hole_pts_dens.size(); i++) {
+          //     std::cout << i + 1 << ": ";
+          //     std::cout << hole_pts_dens[i] << std::endl;
+          //   }
+          // }
 
           for (size_t i = 0; i < (hole_pts_dens.size() - 1); i++) {
             hole_dens.push_back(hole_pts_dens[i]);
           }
-
         }
 
+        // Check for duplicate points in the densified hole boundary
         std::vector<Point> temp_holes;
         for (size_t i = 0; i < hole_dens.size(); i++) {
           temp_holes.push_back(hole_dens[i]);
         }
-
         if (duplicates(temp_holes)) {
-          // for (size_t i = 0; i < temp_out.size(); i++) {
-            std::cout << "Duplicates found in hole!" << std::endl;
-          // }
+          std::cout << "Duplicates found in hole!" << std::endl;
         }
 
         holes_v_dens.push_back(hole_dens);
       }
-
       Polygon_with_holes pgnwh_dens(outer_dens, holes_v_dens.begin(), holes_v_dens.end());
-
       gd_dens.push_back(pgnwh_dens);
     }
-
-    std::cout << "Densified : " << gd.id() << std::endl;
-    // std::cout << polyl.size() << " --> " << polyl_dens.size() << std::endl;
     geodivs_dens.push_back(gd_dens);
   }
+
+  std::cout << "Finished densifying\n";
   return geodivs_dens;
 }
