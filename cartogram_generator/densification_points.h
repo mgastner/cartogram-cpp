bdcc998f270e50541fc8e469881b11cf91b834fc
Phong Le <phongnguyen.le22@gmail.com>
2021-07-16 10:48:40 +0800

    Remove simple_half_ceil and simple_half_floor

--- cartogram_generator/densification_points.h
+++ cartogram_generator/densification_points.h
@@ -6,9 +6,6 @@ std::vector<Point> densification_points(Point a, Point b);
 bool point_almost_equal(Point a, Point b);
 Point round_point(Point p1);
 
-double simple_half_ceil(double num);
-double simple_half_floor(double num);
-
 double half_ceil(double num);
 double half_floor(double num);
 
