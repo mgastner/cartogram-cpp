3b2a6efb279ccfc090e90b28f614252af1f961ab
nihalzp <81457724+nihalzp@users.noreply.github.com>
2021-07-23 10:55:41 +0800

    Replace 0 target area with 0.5 of non-zero minimum

--- cartogram_generator/read_csv.cpp
+++ cartogram_generator/read_csv.cpp
@@ -1,3 +1,4 @@
+#include <cfloat>
 #include "cartogram_info.h"
 #include "inset_state.h"
 #include "csv.hpp"
@@ -18,6 +19,11 @@ void read_csv(const boost::program_options::variables_map vm,
 
   // Open CSV Reader
   csv::CSVReader reader(csv_name);
+
+  // Since it is not allowed to iterate over same csv twice, we initialize
+  // two CSVReader; 
+  // To calculate minimum target area
+  csv::CSVReader reader_area_min(csv_name);
 
   // Find index of column with IDs. If no ID column header was passed with the
   // command-line flag --id, the ID column is assumed to have index 0.
@@ -62,6 +68,15 @@ void read_csv(const boost::program_options::variables_map vm,
     color_col = reader.index_of("Colour");
   }
 
+  // Iterate over target areas and store the minimum target area
+  double area_min = DBL_MAX;
+  for (auto row : reader_area_min) {
+    csv::CSVField area_field = row[area_col];
+    if (area_field.get<double>() != 0) {
+      area_min = std::min(area_min, area_field.get<double>());
+    }
+  }
+
   // Read CSV
   std::set<std::string> inset_pos_set;
   for (auto &row : reader) {
@@ -89,7 +104,9 @@ void read_csv(const boost::program_options::variables_map vm,
     double area;
     if (area_field.is_num()) {
       area = area_field.get<double>();
-      if (area < 0.0) {
+      if (area == 0.0) {
+        area = area_min * 0.5;
+      } else if (area < 0.0) {
         std::cerr << "ERROR: negative area in CSV" << std::endl;
         _Exit(101);
       }
