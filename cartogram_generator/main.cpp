e09df982f91fecc1415e1b21897b1cdf18ad8fdf
adisidev <64905594+adisidev@users.noreply.github.com>
2021-12-20 12:53:01 +0530

    Testing Cairo

--- cartogram_generator/main.cpp
+++ cartogram_generator/main.cpp
@@ -22,9 +22,32 @@
 #include <iostream>
 #include <cmath>
 #include <ranges>
+#include <cairo/cairo.h>
 
 int main(const int argc, const char *argv[])
 {
+
+  cairo_surface_t *surface;
+  cairo_t *cr;
+
+  surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 390, 60);
+  cr = cairo_create(surface);
+
+  cairo_set_source_rgb(cr, 0, 0, 0);
+  cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
+      CAIRO_FONT_WEIGHT_NORMAL);
+  cairo_set_font_size(cr, 40.0);
+
+  cairo_move_to(cr, 10.0, 50.0);
+  cairo_show_text(cr, "Disziplin ist Macht.");
+
+  cairo_surface_write_to_png(surface, "image.png");
+
+  cairo_destroy(cr);
+  cairo_surface_destroy(surface);
+
+  return 0;
+
   std::string geo_file_name, visual_file_name; // Default values
 
   // Default number of grid cells along longer Cartesian coordinate axis.
