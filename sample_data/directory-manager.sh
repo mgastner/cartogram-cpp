4fe778cb2ad864a56e8bc5ba02382c2357f8addb
adisidev <64905594+adisidev@users.noreply.github.com>
2021-12-23 16:49:07 +0530

    Fixed directory-manager.sh, added output

--- sample_data/directory-manager.sh
+++ sample_data/directory-manager.sh
@@ -5,12 +5,12 @@ shopt -s extglob
 for file in *.*json; do
   country="${file%%.*json}"
   country="${country%%_*}"
-  echo=${country}
+  echo "Working on ${country}..."
   mkdir ./${country}/
   mv ${file} ./${country}/${file}
   echo -e "# Sources\n" >> ./${country}/${country}_metadata.md
   echo -e "## ${file}\n\n\n" >> ./${country}/${country}_metadata.md
-  for csv in $(ls ${country}*.csv); do
+  for csv in ${country}*.csv; do
     mv ${csv} ./${country}/${csv}
     echo -e "### ${csv}\n\n\n" >> ./${country}/${country}_metadata.md
   done
