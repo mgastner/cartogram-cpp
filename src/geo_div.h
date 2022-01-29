39fd447cd0e9e9b50523e40d13aed137bff41d2f
mgastner <mgastner@gmail.com>
2022-01-29 20:43:06 +0800

    Sort data members and methods alphabetically

--- src/geo_div.h
+++ src/geo_div.h
@@ -7,9 +7,9 @@
 
 class GeoDiv {
   private:
+    std::set<std::string> adjacent_geodivs_;
     std::string id_;
     std::vector<Polygon_with_holes> polygons_with_holes_;
-    std::set<std::string> adjacent_geodivs_;
     GeoDiv();
 
   public:
@@ -18,15 +18,17 @@ class GeoDiv {
     void adjacent_to(const std::string);
     double area() const;
     const std::string id() const;
+    const std::vector<Segment> intersections(unsigned int) const;
     const Polygon_with_holes largest_polygon_with_holes() const;
     unsigned int n_points() const;
     unsigned int n_polygons_with_holes() const;
     unsigned int n_rings() const;
+    Point point_on_surface_of_geodiv() const;
+    Point point_on_surface_of_polygon_with_holes(
+      const Polygon_with_holes pwh) const;
     const std::vector<Polygon_with_holes> polygons_with_holes() const;
     void push_back(const Polygon_with_holes);
     std::vector<Polygon_with_holes> *ref_to_polygons_with_holes();
-    Point point_on_surface() const;
-
 };
 
 #endif
