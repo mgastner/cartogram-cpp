60e8a1186a7f3b0b47d7ba1c24a0e0a3eafffb81
adisidev <64905594+adisidev@users.noreply.github.com>
2022-02-11 21:18:11 +0800

    Removed almost_equal function in densify.cpp

--- src/check_topology.cpp
+++ src/check_topology.cpp
@@ -31,3 +31,32 @@ void holes_inside_polygons(InsetState *inset_state)
   }
   return;
 }
+
+
+// Use machine epsilon (defined in constants.h) to get almost equal doubles.
+// From https://en.cppreference.com/w/cpp/types/numeric_limits/epsilon
+bool almost_equal(double a, double b) {
+  return abs(a - b) <= dbl_epsilon * abs(a + b) * 2;
+}
+
+// Determine whether points are indistinguishable
+bool points_almost_equal(Point a, Point b) {
+  return (almost_equal(a[0], b[0]) && almost_equal(a[1], b[1]));
+}
+
+bool duplicates(std::vector<Point> v) {
+  CGAL::set_pretty_mode(std::cerr);
+  for (size_t i = 0; i < v.size() - 1; ++i) {
+    if (points_almost_equal(v[i], v[i + 1])) {
+      std::cerr << "i = " << i << std::endl;
+      std::cerr << "Point: " << i << ", v[i]: " << v[i] << std::endl;
+      std::cerr << "Point: "
+                << i + 1
+                << ", v[i + 1]: "
+                << v[i + 1]
+                << std::endl;
+      return true;
+    }
+  }
+  return false;
+}
--- src/inset_state/densify.cpp
+++ src/inset_state/densify.cpp
@@ -1,20 +1,6 @@
 #include "../inset_state.h"
 #include "../constants.h"
-
-// Use machine epsilon (defined in constants.h) to get almost equal doubles.
-// From https://en.cppreference.com/w/cpp/types/numeric_limits/epsilon
-bool almost_equal(double a, double b) {
-  return abs(a - b) <= dbl_epsilon * abs(a + b) * 2;
-}
-
-// Determine whether points are indistinguishable
-bool points_almost_equal(Point a, Point b) {
-  return (almost_equal(a[0], b[0]) && almost_equal(a[1], b[1]));
-}
-
-bool xy_points_almost_equal(XYPoint a, XYPoint b) {
-  return (almost_equal(a.x, b.x) && almost_equal(a.y, b.y));
-}
+#include <CGAL/intersections.h>
 
 // This function takes two lines as input:
 // - line `a`, defined by points a1 and a2.
@@ -22,49 +8,30 @@ bool xy_points_almost_equal(XYPoint a, XYPoint b) {
 // The function returns the intersection between them. If the two lines are
 // parallel or are the same, the function returns the point (-1, -1), which
 // is always outside of any graticule grid cell.
-XYPoint calc_intersection(XYPoint a1, XYPoint a2, XYPoint b1, XYPoint b2) {
+Point calc_intersection(Segment s1, Segment s2) {
 
+  // TODO: I am unsure why this is needed. Duplicate points would be removed
+  // either way.
   // Check if any segment is undefined (i.e., defined by identical points)
-  if (a1 == a2 || b1 == b2) {
-    std::cerr << "ERROR: End points of line segment are identical"
-              << std::endl;
-    _Exit(EXIT_FAILURE);
+  // if (a1 == a2 || b1 == b2) {
+  //   std::cerr << "ERROR: End points of line segment are identical"
+  //             << std::endl;
+  //   _Exit(EXIT_FAILURE);
+  // }
+
+  // Return segment intersection, if it exists
+  const auto result = CGAL::intersection(s1, s2);
+  if (result) {
+    if (const Point* p = boost::get<Point>(&*result)) {
+      return (*p);
+    }
   }
 
-  // Get line equations
-  const double a = (a1.y - a2.y) / (a1.x - a2.x);
-  const double a_intercept = a1.y - (a1.x * a);
-  const double b = (b1.y - b2.y) / (b1.x - b2.x);
-  const double b_intercept = b1.y - (b1.x * b);
-  XYPoint intersection;
-  if (isfinite(a) && isfinite(b) && a != b) {
-
-    // Neither the line (a1, a2) nor the line (b1, b2) is vertical
-    intersection.x = (b_intercept - a_intercept) / (a - b);
-    intersection.y = a * intersection.x + a_intercept;
-  } else if (isfinite(a) && isinf(b)) {
-
-    // Only line (b1, b2) is vertical
-    intersection.x = b1.x;
-    intersection.y = a * b1.x + a_intercept;
-
-  } else if (isfinite(b) && isinf(a)) {
-
-    // Only line (a1, a2) is vertical
-    intersection.x = a1.x;
-    intersection.y = b * a1.x + b_intercept;
-
-  } else {
-
-    // Set negative intersection coordinates if there is no solution or
-    // infinitely many solutions
-    intersection.x = -1;
-    intersection.y = -1;
-  }
-  return intersection;
+  // Value outside grid cell indicating that no intersection was found
+  return Point(-1, -1);
 }
 
-// TODO: If a_ or b_ are themselves intersection points (e.g. if pt1.x is an
+// TODO: If a or b are themselves intersection points (e.g. if pt1.x is an
 // integer plus 0.5), it appears to be included in the returned intersections.
 // Would this property cause the point to be included twice in the line
 // segment (once when the end point is the argument pt1 and a second time when
@@ -79,83 +46,74 @@ XYPoint calc_intersection(XYPoint a1, XYPoint a2, XYPoint b1, XYPoint b2) {
 std::vector<Point> densification_points(Point pt1, Point pt2)
 {
   // Vector for storing intersections before removing duplicates
-  std::vector<XYPoint> temp_intersections;
+  std::vector<Point> temp_intersections;
 
   // Assign `a` as the leftmost point of p1 and pt2. If both points have the
   // same x-coordinate, assign `a` as the lower point. The other point is
   // assigned as b. The segments (a, b) and (b, a) describe the same segment.
   // However, if we flip the order of a and b, the resulting intersections are
   // not necessarily the same because of floating point errors.
-  XYPoint a;
-  XYPoint b;
+  Point a = pt1;
+  Point b = pt2;
   if ((pt1[0] > pt2[0]) || ((pt1[0] == pt2[0]) && (pt1[1] > pt2[1]))) {
-    a.x = pt2[0];
-    a.y = pt2[1];
-    b.x = pt1[0];
-    b.y = pt1[1];
-  } else{
-    a.x = pt1[0];
-    a.y = pt1[1];
-    b.x = pt2[0];
-    b.y = pt2[1];
+    std::swap(a, b);
+    a = pt2;
+    b = pt1;
   }
   temp_intersections.push_back(a);
   temp_intersections.push_back(b);
+  Segment s1(a, b);
 
   // Get bottom-left point of graticule cell containing `a`
-  const XYPoint av0(floor(a.x + 0.5) - 0.5, floor(a.y + 0.5) - 0.5);
+  const Point av0(floor(a.x() + 0.5) - 0.5, floor(a.y() + 0.5) - 0.5);
 
   // Get bottom-left point of graticule cell containing `b`
-  const XYPoint bv0(floor(b.x + 0.5) - 0.5, floor(b.y + 0.5) - 0.5);
+  const Point bv0(floor(b.x() + 0.5) - 0.5, floor(b.y() + 0.5) - 0.5);
 
   // Get bottom-left (start_v) and top-right (end_v) graticule cells of the
   // graticule cell rectangle (the smallest rectangular section of the
   // graticule grid cell containing both points)
-  XYPoint start_v;
-  XYPoint end_v;
-  start_v.x = av0.x;
-  end_v.x = bv0.x;
-  if (a.y <= b.y) {
-    start_v.y = av0.y;
-    end_v.y = bv0.y;
-  } else {
-    start_v.y = bv0.y;
-    end_v.y = av0.y;
+  double y1 = av0.y();
+  double y2 = bv0.y();
+  if (a.y() > b.y()) {
+    std::swap(y1, y2);
   }
+  Point start_v(av0.x(), y1);
+  Point end_v(bv0.x(), y2);
 
   // Loop through each row, from bottom to top
-  for (unsigned int int_y = static_cast<unsigned int>(start_v.y);
-       int_y <= static_cast<unsigned int>(end_v.y);
+  for (unsigned int int_y = static_cast<unsigned int>(start_v.y());
+       int_y <= static_cast<unsigned int>(end_v.y());
        ++int_y) {
 
     // Loop through each column, from left to right
-    for (unsigned int int_x = static_cast<unsigned int>(start_v.x);
-         int_x <= static_cast<unsigned int>(end_v.x);
+    for (unsigned int int_x = static_cast<unsigned int>(start_v.x());
+         int_x <= static_cast<unsigned int>(end_v.x());
          ++int_x) {
 
       // Get points for the current graticule cell, in this order:
       // bottom-left, bottom-right, top-right, top-left
-      const XYPoint v0(int_x + 0.5, int_y + 0.5);
-      const XYPoint v1(v0.x + 1.0, v0.y);
-      const XYPoint v2(v0.x + 1.0, v0.y + 1.0);
-      const XYPoint v3(v0.x, v0.y + 1.0);
-      std::vector<XYPoint> graticule_intersections;
+      const Point v0(int_x + 0.5, int_y + 0.5);
+      const Point v1(v0.x() + 1.0, v0.y());
+      const Point v2(v0.x() + 1.0, v0.y() + 1.0);
+      const Point v3(v0.x(), v0.y() + 1.0);
+      std::vector<Point> graticule_intersections;
 
       // Bottom intersection
-      graticule_intersections.push_back(calc_intersection(a, b, v0, v1));
+      graticule_intersections.push_back(calc_intersection(s1, Segment(v0, v1)));
 
       // Left intersection
-      graticule_intersections.push_back(calc_intersection(a, b, v0, v3));
+      graticule_intersections.push_back(calc_intersection(s1, Segment(v0, v3)));
 
       // Right intersection
-      graticule_intersections.push_back(calc_intersection(a, b, v1, v2));
+      graticule_intersections.push_back(calc_intersection(s1, Segment(v1, v2)));
 
       // Top intersection
-      graticule_intersections.push_back(calc_intersection(a, b, v3, v2));
+      graticule_intersections.push_back(calc_intersection(s1, Segment(v3, v2)));
 
       // Diagonal intersections
-      graticule_intersections.push_back(calc_intersection(a, b, v0, v2));
-      graticule_intersections.push_back(calc_intersection(a, b, v3, v1));
+      graticule_intersections.push_back(calc_intersection(s1, Segment(v0, v2)));
+      graticule_intersections.push_back(calc_intersection(s1, Segment(v3, v1)));
 
       // Add only those intersections that are between `a` and `b`. Usually,
       // it is enough to check that the x-coordinate of the intersection is
@@ -163,11 +121,11 @@ std::vector<Point> densification_points(Point pt1, Point pt2)
       // the x-coordinate is between a.x and b.x, but the y coordinate
       // is not between a.y and b.y (e.g. if the line from a to b is
       // vertical).
-      for (XYPoint inter : graticule_intersections) {
-        if (((a.x <= inter.x && inter.x <= b.x) ||
-             (b.x <= inter.x && inter.x <= a.x)) &&
-            ((a.y <= inter.y && inter.y <= b.y) ||
-             (b.y <= inter.y && inter.y <= a.y))) {
+      for (Point inter : graticule_intersections) {
+        if (((a.x() <= inter.x() && inter.x() <= b.x()) ||
+             (b.x() <= inter.x() && inter.x() <= a.x())) &&
+            ((a.y() <= inter.y() && inter.y() <= b.y()) ||
+             (b.y() <= inter.y() && inter.y() <= a.y()))) {
           temp_intersections.push_back(inter);
         }
       }
@@ -184,35 +142,13 @@ std::vector<Point> densification_points(Point pt1, Point pt2)
 
   // Eliminate duplicates
   std::vector<Point> intersections;
-  intersections.push_back(Point(temp_intersections[0].x,
-                                temp_intersections[0].y));
+  intersections.push_back(temp_intersections[0]);
   for (unsigned int i = 1; i < temp_intersections.size(); ++i) {
-    if (!xy_points_almost_equal(temp_intersections[i - 1],
-                                temp_intersections[i])) {
-      intersections.push_back(Point(temp_intersections[i].x,
-                                    temp_intersections[i].y));
+    if (temp_intersections[i - 1] != temp_intersections[i]) {
+      intersections.push_back(temp_intersections[i]);
     }
   }
   return intersections;
-}
-
-// TODO: This function may be more meaningfully included in
-// check_topology.cpp.
-bool duplicates(std::vector<Point> v) {
-  CGAL::set_pretty_mode(std::cerr);
-  for (size_t i = 0; i < v.size() - 1; ++i) {
-    if (points_almost_equal(v[i], v[i + 1])) {
-      std::cerr << "i = " << i << std::endl;
-      std::cerr << "Point: " << i << ", v[i]: " << v[i] << std::endl;
-      std::cerr << "Point: "
-                << i + 1
-                << ", v[i + 1]: "
-                << v[i + 1]
-                << std::endl;
-      return true;
-    }
-  }
-  return false;
 }
 
 void InsetState::densify_geo_divs()
@@ -245,16 +181,6 @@ void InsetState::densify_geo_divs()
           outer_dens.push_back(outer_pts_dens[i]);
         }
       }
-
-      // Check for duplicate points in the densified outer boundary
-      // std::vector<Point> temp_out;
-      // for (size_t i = 0; i < outer_dens.size(); ++i) {
-      //   temp_out.push_back(outer_dens[i]);
-      // }
-      // if (duplicates(temp_out)) {
-      //   std::cerr << "Duplicates found in outer boundary!" << std::endl;
-      // }
-
       std::vector<Polygon> holes_v_dens;
 
       // Iterate over each hole
@@ -270,16 +196,6 @@ void InsetState::densify_geo_divs()
             hole_dens.push_back(hole_pts_dens[i]);
           }
         }
-
-        // Check for duplicate points in the densified hole boundary
-        // std::vector<Point> temp_holes;
-        // for (size_t i = 0; i < hole_dens.size(); ++i) {
-        //   temp_holes.push_back(hole_dens[i]);
-        // }
-        // if (duplicates(temp_holes)) {
-        //   std::cerr << "Duplicates found in hole!" << std::endl;
-        // }
-
         holes_v_dens.push_back(hole_dens);
       }
       const Polygon_with_holes pwh_dens(outer_dens,
