ca5ab4d1111803f677345c46b3551b2ff6c26f46
Michael T. Gastner <mgastner@gmail.com>
2022-02-12 13:42:22 +0800

    Add TODO

--- src/interpolate_bilinearly.cpp
+++ src/interpolate_bilinearly.cpp
@@ -3,6 +3,8 @@
 
 #include "interpolate_bilinearly.h"
 
+// TODO: REPLACE WITH LINEAR INTERPOLATION BASED ON TRIANGULATION
+
 // Function to bilinearly interpolate a numerical array
 // (*grid)[0..lx-1][0..*ly-1] whose entries are numbers for the positions:
 // x = (0.5, 1.5, ..., lx-0.5), y = (0.5, 1.5, ..., ly-0.5).
@@ -14,10 +16,10 @@
 // determined by continuing the function value at 0.5 (or lx-0.5 or ly-0.5)
 // all the way to the edge (i.e. the slope is 0 consistent with a cosine
 // transform).
-double interpolate_bilinearly(double x,
-                              double y,
+double interpolate_bilinearly(const double x,
+                              const double y,
                               const boost::multi_array<double, 2> *grid,
-                              char zero,
+                              const char zero,
                               const unsigned int lx,
                               const unsigned int ly)
 {
@@ -42,20 +44,20 @@ double interpolate_bilinearly(double x,
 
   // x0 is the nearest grid point smaller than x.
   // Exception: if x < 0.5, x0 becomes 0.0.
-  double x0 = std::max(0.0, floor(x + 0.5) - 0.5);
+  const double x0 = std::max(0.0, floor(x + 0.5) - 0.5);
 
   // x1 is the nearest grid point larger than x.
   // Exception: if x > lx-0.5, x1 becomes lx.
-  double x1 = std::min(static_cast<double>(lx), floor(x + 0.5) + 0.5);
+  const double x1 = std::min(static_cast<double>(lx), floor(x + 0.5) + 0.5);
 
   // Similarly for y
-  double y0 = std::max(0.0, floor(y + 0.5) - 0.5);
-  double y1 = std::min(static_cast<double>(ly), floor(y + 0.5) + 0.5);
+  const double y0 = std::max(0.0, floor(y + 0.5) - 0.5);
+  const double y1 = std::min(static_cast<double>(ly), floor(y + 0.5) + 0.5);
 
   // On a scale from 0 to 1, how far is x (or y) away from x0 (or y0)?
   // 1 means x = x1.
-  double delta_x = (x - x0) / (x1 - x0);
-  double delta_y = (y - y0) / (y1 - y0);
+  const double delta_x = (x - x0) / (x1 - x0);
+  const double delta_y = (y - y0) / (y1 - y0);
 
   // Function value at (x0, y0).
   double fx0y0;
@@ -104,6 +106,8 @@ double interpolate_bilinearly(double x,
   } else {
     fx1y1 = (*grid)[static_cast<int>(x1)][static_cast<int>(y1)];
   }
-  return (1-delta_x)*(1-delta_y)*fx0y0 + (1-delta_x)*delta_y*fx0y1 +
-         delta_x*(1-delta_y)*fx1y0 + delta_x*delta_y*fx1y1;
+  return (1.0-delta_x)*(1.0-delta_y)*fx0y0 +
+         (1.0-delta_x)*delta_y*fx0y1 +
+         delta_x*(1.0-delta_y)*fx1y0 +
+         delta_x*delta_y*fx1y1;
 }
