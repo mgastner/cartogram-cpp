ffab87fea80fd9a0dead72d52771a24b167963bb
nihalzp <81457724+nihalzp@users.noreply.github.com>
2022-02-24 23:08:53 +0800

    Implement time report feature

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,23 @@
 #include "write_cairo.h"
 #include "xy_point.h"
 #include "parse_arguments.h"
+#include <chrono>
+
+typedef  std::chrono::steady_clock::time_point time_point;
+typedef  std::chrono::high_resolution_clock clock_time;
+typedef  std::chrono::milliseconds ms;
+
+template <typename T>
+std::chrono::milliseconds inMilliseconds(T duration)
+{
+    return std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+}
 
 int main(const int argc, const char *argv[])
 {
+  
+  // Start of main function time
+  time_point start_main = clock_time::now();
   std::string geo_file_name, visual_file_name;  // Default values
 
   // Default number of grid cells along longer Cartesian coordinate axis
@@ -83,7 +97,10 @@ int main(const int argc, const char *argv[])
     map_name = map_name.substr(0, map_name.find('.'));
   }
   cart_info.set_map_name(map_name);
-
+  
+  // Parsing start time
+  time_point start_parse = clock_time::now();
+  
   if (!make_csv) {
 
     // Read visual variables (e.g. area, color) from CSV
@@ -121,6 +138,10 @@ int main(const int argc, const char *argv[])
               << std::endl;
     return EXIT_FAILURE;
   }
+  
+  // Parsing end time
+  time_point end_parse = clock_time::now();
+  
   std::cerr << "Coordinate reference system: " << crs << std::endl;
 
   // Progress measured on a scale from 0 (start) to 1 (end)
@@ -134,7 +155,10 @@ int main(const int argc, const char *argv[])
     auto &inset_state = inset_info.second;
     total_geo_divs += inset_state.n_geo_divs();
   }
-
+  
+  // Albers projection start time
+  time_point start_albers_proj = clock_time::now();
+  
   // Project map and ensure that all holes are inside polygons
   for (auto &[inset_pos, inset_state] : *cart_info.ref_to_inset_states()) {
 
@@ -165,10 +189,16 @@ int main(const int argc, const char *argv[])
       return EXIT_FAILURE;
     }
   }
+  
+  // Albers projection end time
+  time_point end_albers_proj = clock_time::now();
 
   // Replace missing and zero target areas with absolute values
   cart_info.replace_missing_and_zero_target_areas();
 
+  // Create map to store duration of each inset integrations
+  std::map<std::string, ms> insets_integration_times;
+  
   // Loop over insets
   for (auto &[inset_pos, inset_state] : *cart_info.ref_to_inset_states()) {
 
@@ -233,8 +263,10 @@ int main(const int argc, const char *argv[])
       // cartogram is proportional to the number of GeoDivs that are in the
       // finished insets
       const double inset_max_frac = inset_state.n_geo_divs() / total_geo_divs;
-
-
+    
+      // Integration start time
+      time_point start_integration = clock_time::now();
+      
       // Start map integration
       while (inset_state.n_finished_integrations() < max_integrations &&
              inset_state.max_area_error().value > max_permitted_area_error) {
@@ -308,6 +340,13 @@ int main(const int argc, const char *argv[])
                   << std::endl
                   << std::endl;
       }
+      
+      // Integration end time
+      time_point end_integration = clock_time::now();
+      
+      // Add integration time to the map
+      insets_integration_times[inset_pos] = inMilliseconds(end_integration -
+                                                    start_integration);
       progress += inset_max_frac;
       std::cerr << "Finished inset "
                 << inset_pos
@@ -372,5 +411,32 @@ int main(const int argc, const char *argv[])
                 std::cout,
                 output_to_stdout,
                 &cart_info);
+                
+  // End of main function time
+  time_point end_main = clock_time::now();
+  
+  // Calculate differences in time
+  ms parsing_time = inMilliseconds(
+    end_parse - start_parse);
+  ms albers_proj_time = inMilliseconds(
+    end_albers_proj - start_albers_proj);
+  ms total_time = inMilliseconds(
+    end_main - start_main);
+  
+  // Show Time Report
+  std::cerr << std::endl;
+  std::cerr << "********** Time Report **********" << std::endl;
+  std::cerr << "Parsing time: " << parsing_time.count() << " ms" << std::endl;
+  std::cerr << "Albers projection time: " << albers_proj_time.count() << " ms" 
+            << std::endl;
+  
+  // Iterate over the map and print integration times  
+  for (auto [inset_pos, inset_integration_time] : insets_integration_times) {
+    std::cerr << "Integration time for inset " << inset_pos << ": "
+              << inset_integration_time.count() << " ms" << std::endl;
+  }
+  std::cerr << "Total time: " << total_time.count() << " ms" << std::endl;
+  std::cerr << "*********************************" << std::endl;
+  
   return EXIT_SUCCESS;
 }
