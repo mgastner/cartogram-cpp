01efb7c9c0eeee020463864a0534159813bd709c
mgastner <mgastner@gmail.com>
2022-03-27 10:46:22 +0800

    Minor stylistic change: 'j<ly_' becomes 'j < ly_'

--- src/inset_state/blur_density.cpp
+++ src/inset_state/blur_density.cpp
@@ -8,7 +8,7 @@ void InsetState::blur_density(const double blur_width, bool plot_density)
   for (unsigned int i = 0; i<lx_; ++i) {
     const double scaled_i = static_cast<double>(i) / lx_;
     const double scaled_i_squared = scaled_i * scaled_i;
-    for (unsigned int j = 0; j<ly_; ++j) {
+    for (unsigned int j = 0; j < ly_; ++j) {
       const double scaled_j = static_cast<double>(j) / ly_;
       const double scaled_j_squared = scaled_j * scaled_j;
       rho_ft_(i, j) *=
