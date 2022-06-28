c2d93146f1fbe1f778151adc642009695be945ef
nihalzp <81457724+nihalzp@users.noreply.github.com>
2022-06-28 19:32:29 +0600

    Round less number of bicimal digits

--- src/inset_state/round_point.cpp
+++ src/inset_state/round_point.cpp
@@ -37,7 +37,7 @@ double rounded_to_bicimal(
 ) {
   double whole;
   const double fractional = std::modf(d, &whole);
-  const unsigned int n_bicimals = 50 - std::bit_width(std::max(lx, ly));
+  const unsigned int n_bicimals = 40 - std::bit_width(std::max(lx, ly));
   const unsigned long int power_of_2 =
     (static_cast<unsigned long int>(1) << n_bicimals);
   const double bicimals = std::round(fractional * power_of_2)  / power_of_2;
