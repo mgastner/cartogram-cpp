a2b8912997373b14322b027d6e01e97ab704fbaa
davidkimbo <davidkimbeomho@gmail.com>
2022-07-26 02:44:17 +0800

    euler step and midpoint method

--- src/inset_state/flatten_ellipse_density.cpp
+++ src/inset_state/flatten_ellipse_density.cpp
@@ -1,5 +1,6 @@
 #include "constants.h"
 #include "inset_state.h"
+#include <cmath>
 
 constexpr double xi_sq(4.0);
 
@@ -99,8 +100,8 @@ double ellipse_density_prefactor(
 
 double ellipse_density_polynomial(double r_tilde_sq)
 {
-  (r_tilde_sq - xi_sq) * (r_tilde_sq - 4 * xi_sq) * (r_tilde_sq - 4 * xi_sq) /
-    (16 * xi_sq * xi_sq * xi_sq);
+  return((r_tilde_sq - xi_sq) * (r_tilde_sq - 4 * xi_sq) * (r_tilde_sq - 4 * xi_sq) /
+    (16 * xi_sq * xi_sq * xi_sq));
 }
 
 double ellipse_flux_prefactor(
@@ -137,8 +138,8 @@ void InsetState::flatten_ellipse_density()
   }
 
   // TODO: Does the next line adjust rho_mean if the total inset area has
-  //       changed in previous runs of flatten_density() or
-  //       flatten_ellipse_density()?
+  //   	changed in previous runs of flatten_density() or
+  //   	flatten_ellipse_density()?
   double rho_mean = total_target_area() / total_inset_area();
   std::cout << "rho_mean = " << rho_mean << std::endl;
 
@@ -146,6 +147,7 @@ void InsetState::flatten_ellipse_density()
   // any ellipse within a fraction f of the mean density.
   double f = 0.1;
   std::vector<Ellipse> ells;
+  std::vector<double> pwh_areas;
   std::vector<double> ell_density_prefactors;
   std::vector<double> rho_p_vec;
   for (auto gd : geo_divs_) {
@@ -157,6 +159,7 @@ void InsetState::flatten_ellipse_density()
       for (auto h = pwh.holes_begin(); h != pwh.holes_end(); ++h) {
         pwh_area += h->area();
       }
+      pwh_areas.push_back(pwh_area);
       Ellipse ell = gd.min_ellipses()[pgon];
       ells.push_back(ell);
       ell_density_prefactors.push_back(
@@ -164,6 +167,7 @@ void InsetState::flatten_ellipse_density()
       rho_p_vec.push_back(rho_p);
     }
   }
+
   double rho_min = *std::min_element(
     ell_density_prefactors.begin(),
     ell_density_prefactors.end());
@@ -172,8 +176,9 @@ void InsetState::flatten_ellipse_density()
     ell_density_prefactors.end());
   std::cout << "rho_min = " << rho_min << ", rho_max = " << rho_max
             << std::endl;
+
   double nu = 1.0;
-  double acceptable_min = -f * rho_mean;
+  double acceptable_min = -f * rho_mean; // f = 0.1
   double acceptable_max = f * rho_mean;
   if (rho_min < acceptable_min || rho_max > acceptable_max) {
     double nu_min = acceptable_min / rho_min;
@@ -183,26 +188,39 @@ void InsetState::flatten_ellipse_density()
     }
   }
   std::cout << "nu = " << nu << std::endl;
-  for (unsigned int pgn_index; pgn_index < ell_density_prefactors.size();
-       ++pgn_index) {
+
+  for (unsigned int pgn_index = 0; pgn_index < ell_density_prefactors.size();
+        ++pgn_index) {
     ell_density_prefactors[pgn_index] *= nu;
   }
+  rho_min = *std::min_element(
+    ell_density_prefactors.begin(),
+    ell_density_prefactors.end());
+  rho_max = *std::max_element(
+    ell_density_prefactors.begin(),
+    ell_density_prefactors.end());
+  std::cout << "rho_min_post = " << rho_min << ", rho_max_post = " << rho_max
+            << std::endl;
 
   // Initial time and step size
   double t = 0.0;
   double delta_t = 1e-2;  // Initial time step.
-  unsigned int iter = 0;
+  unsigned int iter = 0;  // Counter for the number of iterations
+  bool accepted = false;
+
 
   // Integrate
   while (t < 1.0) {
     for (const auto &[start_pt, curr_pt] : proj_qd_.triangle_transformation) {
-      double rho = 0.0;
+      double rho = rho_mean; 
       double flux_x = 0.0;
       double flux_y = 0.0;
 
-      // Calculate density, flux and velocity at curr_pt
-      for (unsigned int pgn_index; pgn_index < ells.size(); ++pgn_index) {
+      // Calculate density, flux and velocity at curr_pt and current time
+      for (unsigned int pgn_index = 0; pgn_index < ells.size(); ++pgn_index) {
         auto ell = ells[pgn_index];
+        auto pwh_area = pwh_areas[pgn_index];
+        auto rho_p = rho_p_vec[pgn_index];
         for (int i = -2; i <= 2; ++i) {
           double x = ((i + abs(i) % 2) * static_cast<int>(lx_)) +
                      (curr_pt.x() * (i % 2 == 0 ? 1 : -1));
@@ -211,165 +229,127 @@ void InsetState::flatten_ellipse_density()
                        (curr_pt.y() * (j % 2 == 0 ? 1 : -1));
             double x_tilde = ((x - ell.center.x()) * ell.cos_theta +
                               (y - ell.center.y()) * ell.sin_theta) /
-                             ell.semimajor;
+                              ell.semimajor;
             double y_tilde = ((-(x - ell.center.x()) * ell.sin_theta) +
                               (y - ell.center.y()) * ell.cos_theta) /
-                             ell.semiminor;
+                              ell.semiminor;
             double r_tilde_sq = (x_tilde * x_tilde) + (y_tilde * y_tilde);
-            rho += ell_density_prefactors[pgn_index] *
-                   ellipse_density_polynomial(r_tilde_sq);
-//            double ell_flux_prefactor ellipse_flux_prefactor(ell, r_tilde_sq,
-//              double rho_p,
-//              double rho_mean,
-//              double pwh_area,
-//              double nu)
-//            fluxx =
+            rho += (ell_density_prefactors[pgn_index] *
+                    ellipse_density_polynomial(r_tilde_sq)) *
+                    (1.0 - t);
+            double prefac = ellipse_flux_prefactor(
+              ell,
+              r_tilde_sq,
+              rho_p,
+              rho_mean,
+              pwh_area,
+              nu);
+            double flux_tilde_x = prefac * x_tilde;
+            double flux_tilde_y = prefac * y_tilde;
+            flux_x += ell.semimajor * flux_tilde_x * ell.cos_theta -
+                      ell.semiminor * flux_tilde_y * ell.sin_theta;
+            flux_y += ell.semimajor * flux_tilde_x * ell.sin_theta +
+                      ell.semiminor * flux_tilde_y * ell.cos_theta;
           }
         }
       }
+      //Velocity values
+      double v_x = flux_x / rho;
+      double v_y = flux_y / rho;
+      accepted = false;
+      while (!accepted) {
+        //Euler value
 
-      // `eul` will be the new position of proj_[i][j] proposed by a simple
-      // Euler step: move a full time interval delta_t with the velocity at
-      // time t and position (proj_[i][j].x, proj_[i][j].y)
+        Point eul_val(
+          curr_pt.x() + v_x * delta_t,
+          curr_pt.y() + v_y * delta_t);
 
-      // `mid` will be the new displacement proposed by the midpoint
-      // method (see comment below for the formula)
-      return;
+      	// Explicit midpoint method:
+      	// x <- x + delta_t * v_x(x + 0.5*delta_t*v_x(x,y,t),
+    	  //                    	y + 0.5*delta_t*v_y(x,y,t),
+    	  //                    	t + 0.5*delta_t)
+        
+        // Calculating the velocity values needed to find the midpoint as per the midpoint method
+        double v_x_t = curr_pt.x() + 0.5 * delta_t * v_x; // x value for new velocity
+        double v_y_t = curr_pt.y() + 0.5 * delta_t * v_y; // y value for new velocity
+        double t_mid = t + 0.5 * delta_t;
+
+        //Carry out same steps as above to find velocity values
+
+        double rho_mid = 0.0;
+        double flux_x_mid = 0.0;
+        double flux_y_mid = 0.0;
+        for (unsigned int pgn_index_mid = 0; pgn_index_mid < ells.size();
+              ++pgn_index_mid) {
+          auto ell_mid = ells[pgn_index_mid];
+          auto pwh_area_mid = pwh_areas[pgn_index_mid];
+          auto rho_p_mid = rho_p_vec[pgn_index_mid];
+          for (int i = -2; i <= 2; ++i) {
+            double x = ((i + abs(i) % 2) * static_cast<int>(lx_)) +
+                       (v_x_t * (i % 2 == 0 ? 1 : -1));
+            for (int j = -2; j <= 2; ++j) {
+              double y = ((j + abs(j) % 2) * static_cast<int>(ly_)) +
+                         (v_y_t * (j % 2 == 0 ? 1 : -1));
+              double x_tilde_mid =
+                ((x - ell_mid.center.x()) * ell_mid.cos_theta +
+                 (y - ell_mid.center.y()) * ell_mid.sin_theta) /
+                ell_mid.semimajor;
+              double y_tilde_mid =
+                ((-(x - ell_mid.center.x()) * ell_mid.sin_theta) +
+                 (y - ell_mid.center.y()) * ell_mid.cos_theta) /
+                ell_mid.semiminor;
+              double r_tilde_sq_mid =
+                (x_tilde_mid * x_tilde_mid) + (y_tilde_mid * y_tilde_mid);
+              rho_mid += (ell_density_prefactors[pgn_index_mid] *
+                          ellipse_density_polynomial(r_tilde_sq_mid)) *
+                          (1.0 - t_mid);
+              double prefac_mid = ellipse_flux_prefactor(
+                ell_mid,
+                r_tilde_sq_mid,
+                rho_p_mid,
+                rho_mean,
+                pwh_area_mid,
+                nu);
+              double flux_tilde_x_mid = prefac_mid * x_tilde_mid;
+              double flux_tilde_y_mid = prefac_mid * y_tilde_mid;
+              flux_x_mid +=
+                ell_mid.semimajor * flux_tilde_x_mid * ell_mid.cos_theta -
+                ell_mid.semiminor * flux_tilde_y_mid * ell_mid.sin_theta;
+              flux_y_mid +=
+                ell_mid.semimajor * flux_tilde_x_mid * ell_mid.sin_theta +
+                ell_mid.semiminor * flux_tilde_y_mid * ell_mid.cos_theta;
+            }
+          }
+        }
+        double v_x_t_mid = flux_x_mid / rho_mid;
+        double v_y_t_mid = flux_y_mid / rho_mid;
+
+        //New displacement proposed by the midpoint method
+        double mid_x = curr_pt.x() + delta_t * v_x_t_mid;
+        double mid_y = curr_pt.y() + delta_t * v_y_t_mid;
+
+        const double sq_dist = (mid_x - eul_val.x()) * (mid_x - eul_val.x()) +
+                               (mid_y - eul_val.y()) * (mid_y - eul_val.y());
+
+        //Checking if the sq_dist is within the tolerance and within the x and y limits
+        if (sq_dist <= abs_tol && mid_x >= 0 && mid_x <= lx_ && mid_y >= 0 && mid_y <= ly_) {  
+          accepted = true;
+          proj_qd_.triangle_transformation[start_pt] = Point(mid_x, mid_y);
+          delta_t *= inc_after_acc;
+          std::cout << "delta: " << start_pt.x() - mid_x << ", " << start_pt.y() - mid_y << std::endl;
+        }
+        else{
+          delta_t *= dec_after_not_acc;
+        }
+        
+
+      
+      }
+      std::cout << "delta_t: " << delta_t << std::endl;
     }
+    iter++;
+    t += delta_t;
+    std::cout << "Number of iterations: " << iter << std::endl;
+    std::cout << "t = " << t << std::endl;
   }
-  //    calculate_velocity(
-  //      t,
-  //      grid_fluxx_init,
-  //      grid_fluxy_init,
-  //      rho_ft_,
-  //      rho_init_,
-  //      &grid_vx,
-  //      &grid_vy,
-  //      lx_,
-  //      ly_);
-  //
-  //
-  //      // We know, either because of the initialization or because of the
-  //      // check at the end of the last iteration, that (proj_.x, proj_.y)
-  //      // is inside the rectangle [0, lx_] x [0, ly_]. This fact
-  //      guarantees
-  //      // that interpolate_bilinearly() is given a point that cannot cause
-  //      it
-  //      // to fail.
-  //      Point v_intp_val(
-  //        interpolate_bilinearly(val.x(), val.y(), &grid_vx, 'x', lx_,
-  //        ly_), interpolate_bilinearly(val.x(), val.y(), &grid_vy, 'y',
-  //        lx_, ly_));
-  //      v_intp.insert_or_assign(key, v_intp_val);
-  //    }
-  //
-  //    bool accept = false;
-  //    while (!accept) {
-  //
-  //      // Simple Euler step.
-  //      for (const auto &[key, val] : proj_qd_.triangle_transformation) {
-  //        Point eul_val(
-  //          val.x() + v_intp[key].x() * delta_t,
-  //          val.y() + v_intp[key].y() * delta_t);
-  //        eul.insert_or_assign(key, eul_val);
-  //      }
-  //
-  //      // Use "explicit midpoint method"
-  //      // x <- x + delta_t * v_x(x + 0.5*delta_t*v_x(x,y,t),
-  //      //                        y + 0.5*delta_t*v_y(x,y,t),
-  //      //                        t + 0.5*delta_t)
-  //      // and similarly for y.
-  //      calculate_velocity(
-  //        t + 0.5 * delta_t,
-  //        grid_fluxx_init,
-  //        grid_fluxy_init,
-  //        rho_ft_,
-  //        rho_init_,
-  //        &grid_vx,
-  //        &grid_vy,
-  //        lx_,
-  //        ly_);
-  //
-  //      // Make sure we do not pass a point outside [0, lx_] x [0, ly_] to
-  //      // interpolate_bilinearly(). Otherwise decrease the time step below
-  //      and
-  //      // try again.
-  //      accept = all_map_points_are_in_domain(
-  //        delta_t,
-  //        &proj_qd_.triangle_transformation,
-  //        &v_intp,
-  //        lx_,
-  //        ly_);
-  //      if (accept) {
-  //
-  //        // Okay, we can run interpolate_bilinearly()
-  //        for (const auto &[key, val] : proj_qd_.triangle_transformation) {
-  //          Point v_intp_half_val(
-  //            interpolate_bilinearly(
-  //              val.x() + 0.5 * delta_t * v_intp[key].x(),
-  //              val.y() + 0.5 * delta_t * v_intp[key].y(),
-  //              &grid_vx,
-  //              'x',
-  //              lx_,
-  //              ly_),
-  //            interpolate_bilinearly(
-  //              val.x() + 0.5 * delta_t * v_intp[key].x(),
-  //              val.y() + 0.5 * delta_t * v_intp[key].y(),
-  //              &grid_vy,
-  //              'y',
-  //              lx_,
-  //              ly_));
-  //          v_intp_half.insert_or_assign(key, v_intp_half_val);
-  //          Point mid_val(
-  //            val.x() + v_intp_half[key].x() * delta_t,
-  //            val.y() + v_intp_half[key].y() * delta_t);
-  //          mid.insert_or_assign(key, mid_val);
-  //
-  //          // Do not accept the integration step if the maximum squared
-  //          // difference between the Euler and midpoint proposals exceeds
-  //          // abs_tol. Neither should we accept the integration step if
-  //          one
-  //          // of the positions wandered out of the domain. If one of these
-  //          // problems occurred, decrease the time step.
-  //          const double sq_dist =
-  //            (mid[key].x() - eul[key].x()) * (mid[key].x() - eul[key].x())
-  //            + (mid[key].y() - eul[key].y()) * (mid[key].y() -
-  //            eul[key].y());
-  //          if (
-  //            sq_dist > abs_tol || mid[key].x() < 0.0 || mid[key].x() > lx_
-  //            || mid[key].y() < 0.0 || mid[key].y() > ly_) { accept =
-  //            false;
-  //          }
-  //        }
-  //      }
-  //      if (!accept) {
-  //        delta_t *= dec_after_not_acc;
-  //      }
-  //    }
-  //
-  //    // Control ouput
-  //    if (iter % 10 == 0) {
-  //      std::cerr << "iter = " << iter << ", t = " << t
-  //                << ", delta_t = " << delta_t << "\n";
-  //    }
-  //
-  //    // When we get here, the integration step was accepted
-  //    t += delta_t;
-  //    ++iter;
-  //
-  //    // Update the triangle transformation map
-  //    proj_qd_.triangle_transformation = mid;
-  //    delta_t *= inc_after_acc;  // Try a larger step next time
-  //  }
-  //
-  //  // Add current proj to proj_sequence vector
-  //  proj_sequence_.push_back(proj_qd_);
-  //
-  //  // Clean up
-  //  grid_fluxx_init.destroy_fftw_plan();
-  //  grid_fluxy_init.destroy_fftw_plan();
-  //  grid_fluxx_init.free();
-  //  grid_fluxy_init.free();
-
-  return;
 }
