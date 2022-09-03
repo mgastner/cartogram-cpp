cae7f9c14efdade53420c6de9c0fb2aaae32cf21
nihalzp <81457724+nihalzp@users.noreply.github.com>
2022-09-04 00:27:33 +0800

    Add new data member "theta" in Ellipse struct

--- include/ellipse.h
+++ include/ellipse.h
@@ -7,6 +7,7 @@ struct Ellipse {
   double semimajor;
   double semiminor;
   Point center;
+  double theta;
 
   // To avoid calculating trigonometric functions, we do not directly store
   // the angle theta between the x-axis and the semimajor axis. Instead we
