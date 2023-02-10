9d4a36f1a32d9508867a2eefa0f39b6306fbec6a
nihalzp <81457724+nihalzp@users.noreply.github.com>
2023-02-10 17:26:15 +0800

    Comment out pragma in set_area_errors

--- src/inset_state/inset_state.cpp
+++ src/inset_state/inset_state.cpp
@@ -402,7 +402,7 @@ void InsetState::set_area_errors()
   double sum_target_area = 0.0;
   double sum_cart_area = 0.0;
 
-#pragma omp parallel for default(none) reduction(+ : sum_target_area, sum_cart_area)
+// #pragma omp parallel for default(none) reduction(+ : sum_target_area, sum_cart_area)
   for (const auto &gd : geo_divs_) {
     sum_target_area += target_area_at(gd.id());
     sum_cart_area += gd.area();
