d08f8d75e8b513f3dfd64d56e752e437dcc60476
nihalzp <81457724+nihalzp@users.noreply.github.com>
2023-07-01 09:46:47 +0800

    Make flatten ellipse density more robust

--- src/inset_state/flatten_ellipse_density.cpp
+++ src/inset_state/flatten_ellipse_density.cpp
@@ -1,17 +1,13 @@
 #include "constants.h"
 #include "inset_state.h"
 
-// Get GeoDiv name from polygon id
-std::vector<std::string> pgn_to_geo;
-
 void InsetState::min_ellipses()
 {
   for (auto &gd : geo_divs_) {
     gd.clear_min_ellipses();
     for (auto &pwh : gd.polygons_with_holes()) {
       auto ext_ring_actual = pwh.outer_boundary();
-      std::vector<double> x_coords;
-      std::vector<double> y_coords;
+      std::vector<double> x_coords, y_coords;
       for (const auto &pt : ext_ring_actual) {
         x_coords.push_back(pt.x());
         y_coords.push_back(pt.y());
@@ -24,10 +20,10 @@ void InsetState::min_ellipses()
         y_coords.begin(),
         y_coords.begin() + y_coords.size() / 2,
         y_coords.end());
-        
-      double x_median = x_coords[x_coords.size() / 2];
-      double y_median = y_coords[y_coords.size() / 2];
-      
+
+      const double x_median = x_coords[x_coords.size() / 2];
+      const double y_median = y_coords[y_coords.size() / 2];
+
       // Shift the polygon so that its median is at the origin
       // This is necessary to avoid floating point errors
       Polygon ext_ring;
@@ -44,7 +40,7 @@ void InsetState::min_ellipses()
       if (ext_ring.size() < 6) {
         Min_circle mc(ext_ring.vertices_begin(), ext_ring.vertices_end());
         ell.center = mc.circle().center();
-        
+
         // Shift the center back to the original coordinate system
         ell.center =
           Point(ell.center.x() + x_median, ell.center.y() + y_median);
@@ -76,16 +72,16 @@ void InsetState::min_ellipses()
           e *= -1;
           f *= -1;
         }
-        double denom = (b * b) - (4 * a * c);
-        double fac1 =
+        const double denom = (b * b) - (4 * a * c);
+        const double fac1 =
           (a * e * e) + (c * d * d) - (b * d * e) + ((b * b - 4 * a * c) * f);
-        double inner_sqrt = sqrt(((a - c) * (a - c)) + (b * b));
+        const double inner_sqrt = sqrt(((a - c) * (a - c)) + (b * b));
         ell.semimajor = -sqrt(2 * fac1 * (a + c + inner_sqrt)) / denom;
         ell.semiminor = -sqrt(2 * fac1 * (a + c - inner_sqrt)) / denom;
         ell.center = Point(
           ((2 * c * d) - (b * e)) / denom,
           ((2 * a * e) - (b * d)) / denom);
-          
+
         // Shift the center back to the original coordinate system
         ell.center =
           Point(ell.center.x() + x_median, ell.center.y() + y_median);
@@ -104,15 +100,15 @@ void InsetState::min_ellipses()
 }
 
 double ellipse_density_prefactor(
-  double rho_p,
-  double rho_mean,
-  double pwh_area,
-  double nu)
+  const double &rho_p,
+  const double &rho_mean,
+  const double &pwh_area,
+  const double &nu)
 {
   return nu * pwh_area * (rho_p - rho_mean) / pi;
 }
 
-double ellipse_density_polynomial(double r_tilde_sq)
+double ellipse_density_polynomial(const double &r_tilde_sq)
 {
   if (r_tilde_sq >= 4 * xi_sq)
     return 0.0;
@@ -122,32 +118,39 @@ double ellipse_density_polynomial(double r_tilde_sq)
 }
 
 double ellipse_flux_prefactor(
-  double r_tilde_sq,
-  double rho_p,
-  double rho_mean,
-  double pwh_area,
-  double nu)
+  const double &r_tilde_sq,
+  const double &rho_p,
+  const double &rho_mean,
+  const double &pwh_area,
+  const double &nu)
 {
   if (r_tilde_sq >= 4 * xi_sq)
     return 0.0;
-  double xi_to_6 = xi_sq * xi_sq * xi_sq;
+  const double xi_to_6 = xi_sq * xi_sq * xi_sq;
   return nu * pwh_area * (rho_p - rho_mean) * (4 * xi_sq - r_tilde_sq) *
          (4 * xi_sq - r_tilde_sq) * (4 * xi_sq - r_tilde_sq) /
          (128 * pi * xi_to_6);
 }
 
 bool all_map_points_are_in_domain(
-  double delta_t,
-  std::unordered_map<Point, Point> &proj_map,
-  std::unordered_map<Point, Point> &v_intp,
-  const unsigned int lx,
-  const unsigned int ly)
+  const double &delta_t,
+  const std::unordered_map<Point, Point> &proj_map,
+  const std::unordered_map<Point, Point> &v_intp,
+  const unsigned int &lx,
+  const unsigned int &ly)
 {
   // Return false if and only if there exists a point that would be outside
   // [0, lx] x [0, ly]
-  for (auto &[key, val] : proj_map) {
-    double x = val.x() + 0.5 * delta_t * v_intp[key].x();
-    double y = val.y() + 0.5 * delta_t * v_intp[key].y();
+  for (const auto &[key, val] : proj_map) {
+    double x = val.x() + 0.5 * delta_t * v_intp.at(key).x();
+    double y = val.y() + 0.5 * delta_t * v_intp.at(key).y();
+
+    // if close to 0 using EPS, make 0, or greater than lx or ly, make lx or ly
+    if (abs(x) < dbl_epsilon || abs(x - lx) < dbl_epsilon)
+      x = (abs(x) < dbl_epsilon) ? 0 : lx;
+    if (abs(y) < dbl_epsilon || abs(y - ly) < dbl_epsilon)
+      y = (abs(y) < dbl_epsilon) ? 0 : ly;
+
     if (x < 0.0 || x > lx || y < 0.0 || y > ly) {
       return false;
     }
@@ -156,26 +159,30 @@ bool all_map_points_are_in_domain(
 }
 
 void calculate_velocity(
-  std::unordered_map<Point, double> &rho_mp,
-  std::unordered_map<Point, Point> &flux_mp,
-  std::unordered_map<Point, Point> &triangle_transformation,
+  const std::unordered_map<Point, double> &rho_mp,
+  const std::unordered_map<Point, Point> &flux_mp,
+  const std::unordered_map<Point, Point> &triangle_transformation,
   std::unordered_map<Point, Point> &velocity)
 {
   for (const auto &[key, val] : triangle_transformation) {
-    velocity[key] =
-      Point(flux_mp[key].x() / rho_mp[key], flux_mp[key].y() / rho_mp[key]);
+    velocity[key] = Point(
+      flux_mp.at(key).x() / rho_mp.at(key),
+      flux_mp.at(key).y() / rho_mp.at(key));
   }
 }
 
-Point interpolate(Point p, Delaunay &dt, std::unordered_map<Point, Point> &mp)
+Point interpolate(
+  const Point p,
+  const Delaunay &dt,
+  const std::unordered_map<Point, Point> &mp)
 {
   // Find the triangle containing the point
-  Face_handle fh = dt.locate(p);
+  const Face_handle fh = dt.locate(p);
 
   // Get three vertices
-  Point v1 = fh->vertex(0)->point();
-  Point v2 = fh->vertex(1)->point();
-  Point v3 = fh->vertex(2)->point();
+  const Point v1 = fh->vertex(0)->point();
+  const Point v2 = fh->vertex(1)->point();
+  const Point v3 = fh->vertex(2)->point();
 
   // Calculate barycentric coordinates
   std::tuple<Scd::FT, Scd::FT, Scd::FT> bary_coor;
@@ -192,21 +199,23 @@ Point interpolate(Point p, Delaunay &dt, std::unordered_map<Point, Point> &mp)
   const double bary_z = std::get<2>(bary_coor);
 
   // Get projected vertices
-  auto v1_proj = mp[v1];
-  auto v2_proj = mp[v2];
-  auto v3_proj = mp[v3];
+  const Point v1_proj = mp.at(v1);
+  const Point v2_proj = mp.at(v2);
+  const Point v3_proj = mp.at(v3);
 
   // Calculate projected point of p
   const Point p_proj = Point(
     bary_x * v1_proj.x() + bary_y * v2_proj.x() + bary_z * v3_proj.x(),
     bary_x * v1_proj.y() + bary_y * v2_proj.y() + bary_z * v3_proj.y());
-
   return p_proj;
 }
 
 void InsetState::flatten_ellipse_density()
 {
   std::cerr << "In flatten_ellipse_density()" << std::endl;
+
+  // Get GeoDiv name from polygon id
+  std::vector<std::string> pgn_id_to_geo_id;
 
   std::unordered_map<Point, double> rho_mp;
   std::unordered_map<Point, Point> flux_mp;
@@ -246,8 +255,6 @@ void InsetState::flatten_ellipse_density()
   // TODO: Does the next line adjust rho_mean if the total inset area has
   //       changed in previous runs of flatten_density() or
   //       flatten_ellipse_density()?
-  // double rho_mean = total_target_area() / total_inset_area();
-  
   double rho_mean = (1.0 - (initial_area_ / (lx_ * ly_))) /
                     (1.0 - (total_inset_area() / (lx_ * ly_)));
 
@@ -260,85 +267,80 @@ void InsetState::flatten_ellipse_density()
   std::vector<double> pwh_rhos;
   std::vector<double> pwh_areas;
 
-  for (auto &gd : geo_divs_) {
-    double rho_p = (target_area_at(gd.id()) / gd.area());
+  for (const auto &gd : geo_divs_) {
+    const double rho_p = (target_area_at(gd.id()) / gd.area());
     for (unsigned int pgon = 0; pgon < gd.n_polygons_with_holes(); ++pgon) {
       pwh_rhos.push_back(rho_p);
     }
   }
 
   for (auto &gd : geo_divs_) {
-    double rho_p = (target_area_at(gd.id()) / gd.area());
+    const double rho_p = (target_area_at(gd.id()) / gd.area());
     for (unsigned int pgon = 0; pgon < gd.n_polygons_with_holes(); ++pgon) {
       Polygon_with_holes pwh = gd.polygons_with_holes()[pgon];
-      const auto ext_ring = pwh.outer_boundary();
-      double pwh_area = ext_ring.area();
-      for (auto h = pwh.holes_begin(); h != pwh.holes_end(); ++h) {
-        pwh_area += h->area();
-      }
-      pwh_areas.push_back(pwh_area);
-      Ellipse ell = gd.min_ellipses()[pgon];
+      const double area = pwh_area(pwh);
+      pwh_areas.push_back(area);
+      const Ellipse ell = gd.min_ellipses()[pgon];
       ells.push_back(ell);
       ell_density_prefactors.push_back(
-        ellipse_density_prefactor(rho_p, rho_mean, pwh_area, nu) /
+        ellipse_density_prefactor(rho_p, rho_mean, area, nu) /
         (ell.semimajor * ell.semiminor));
-        
-      // Fill pgn_to_geo
-      pgn_to_geo.push_back(gd.id());
+
+      // Fill GeoDiv name in the same order
+      pgn_id_to_geo_id.push_back(gd.id());
     }
   }
 
-  double rho_min = *std::min_element(
+  double delta_min = *std::min_element(
     ell_density_prefactors.begin(),
     ell_density_prefactors.end());
-  double rho_max = *std::max_element(
+  double delta_max = *std::max_element(
     ell_density_prefactors.begin(),
     ell_density_prefactors.end());
 
-  std::cerr << "delta_min = " << rho_min << ", delta_max = " << rho_max
+  std::cerr << "delta_min = " << delta_min << ", delta_max = " << delta_max
             << std::endl;
   std::cerr << "rho_mean = " << rho_mean << std::endl;
-
-  // cerr previous nu
   std::cerr << "previous nu = " << nu << std::endl;
+  
   // Update the prefactor densities
   double acceptable_max = nu * rho_mean;
-  if (rho_max > acceptable_max) {
-    double nu_max = acceptable_max / rho_max;
+  if (delta_max > acceptable_max) {
+    double nu_max = acceptable_max / delta_max;
     if (nu_max < 1.0) {
       nu = nu_max;
     }
   }
-  
-  // find the print the polygon name with max and min density, and the densities
+
+  // find the print the polygons with max delta density
   int mx_pgn_index = std::max_element(
-    ell_density_prefactors.begin(),
-    ell_density_prefactors.end()) -
+                       ell_density_prefactors.begin(),
+                       ell_density_prefactors.end()) -
                      ell_density_prefactors.begin();
-                     
+
   std::cerr << "Max delta rho: " << ell_density_prefactors[mx_pgn_index]
-            << ", GeoDiv: " << pgn_to_geo[mx_pgn_index] << std::endl;
-            
-  // print top 5 polygons with most delta rho
-  // make vector of pairs <density, polygon name>
+            << ", GeoDiv: " << pgn_id_to_geo_id[mx_pgn_index] << std::endl;
+
+  // print top 5 polygons with most delta density
   std::vector<std::pair<double, std::string>> pgn_density;
   for (unsigned int i = 0; i < ell_density_prefactors.size(); ++i) {
     pgn_density.push_back(
-      std::make_pair(ell_density_prefactors[i], pgn_to_geo[i]));
+      std::make_pair(ell_density_prefactors[i], pgn_id_to_geo_id[i]));
   }
-  // sort the vector
+
+  // Sort in descending order
   std::sort(
     pgn_density.begin(),
     pgn_density.end(),
     std::greater<std::pair<double, std::string>>());
-    
-  // print the top 5
-  std::cerr << "Top 5 Polygons with most Delta Rho:" << std::endl;
+
+  // Print the top 5
+  std::cerr << "Top 5 Polygons with most Delta density:" << std::endl;
   for (int i = 0; i < std::min(5, (int)ell_density_prefactors.size()); ++i) {
     std::cerr << pgn_density[i].second << ": " << pgn_density[i].first
               << std::endl;
   }
-  
+
   std::cerr << "Adjusted nu = " << nu << std::endl;
   for (unsigned int pgn_index = 0; pgn_index < ell_density_prefactors.size();
        ++pgn_index) {
@@ -348,7 +350,7 @@ void InsetState::flatten_ellipse_density()
   // Calculate densities
   for (const auto &[start_pt, curr_pt] : proj_qd_.triangle_transformation) {
     double rho =
-      rho_mean;  // to avoid division by zero while calculating velocity
+      rho_mean;
     double flux_x = 0.0;
     double flux_y = 0.0;
     for (unsigned int pgn_index = 0; pgn_index < ells.size(); ++pgn_index) {
@@ -390,32 +392,6 @@ void InsetState::flatten_ellipse_density()
   double t = 0.0;
   double delta_t = 1e-2;  // Initial time step.
   unsigned int iter = 0;
-  
-  // print the corner points flux of flux_mp
-  // std::cerr << "flux_mp corner points:" << std::endl;
-  // for (const auto &[key, val] : proj_qd_.triangle_transformation) {
-  //   // make 0
-  //   if(key.x() == 0 || key.x() == lx_ || key.y() == 0 || key.y() == ly_) {
-  //    // if close to 0 using EPS
-  //    if(abs(val.x()) < 1e-9) flux_mp[key] = Point(0, flux_mp[key].y());
-  //    if(abs(val.y()) < 1e-9) flux_mp[key] = Point(flux_mp[key].x(), 0);
-  //   }
-  // }
-
-  // write_cairo_map(
-  //   inset_name() + "_" + std::to_string(n_finished_integrations()) +
-  //   "_flux", false, flux_mp);
-
-  // calculate_velocity(
-  //   rho_mp,
-  //   flux_mp,
-  //   proj_qd_.triangle_transformation,
-  //   velocity);
-  // write_cairo_map(
-  //   inset_name() + "_" + std::to_string(n_finished_integrations()) +
-  //     "_velcity",
-  //   false,
-  //   velocity);
 
   while (t < 1.0) {
     calculate_velocity(
@@ -431,14 +407,6 @@ void InsetState::flatten_ellipse_density()
     }
     bool accept = false;
     do {
-      // Simple Euler step.
-      for (const auto &[key, val] : proj_qd_.triangle_transformation) {
-        Point eul_val(
-          val.x() + v_intp[key].x() * delta_t,
-          val.y() + v_intp[key].y() * delta_t);
-        eul.insert_or_assign(key, eul_val);
-      }
-
       calculate_velocity(
         rho_mp,
         flux_mp,
@@ -453,15 +421,33 @@ void InsetState::flatten_ellipse_density()
         ly_);
 
       if (accept) {
+
+        // Simple Euler step.
+        for (const auto &[key, val] : proj_qd_.triangle_transformation) {
+          Point eul_val(
+            val.x() + v_intp[key].x() * delta_t,
+            val.y() + v_intp[key].y() * delta_t);
+          eul.insert_or_assign(key, eul_val);
+        }
+
         for (const auto &[key, val] : proj_qd_.triangle_transformation) {
           Point n_val = Point(
             val.x() + 0.5 * delta_t * v_intp[key].x(),
             val.y() + 0.5 * delta_t * v_intp[key].y());
           Point v_intp_half_val(interpolate(n_val, proj_qd_.dt, velocity));
           v_intp_half.insert_or_assign(key, v_intp_half_val);
-          Point mid_val(
-            val.x() + v_intp_half[key].x() * delta_t,
-            val.y() + v_intp_half[key].y() * delta_t);
+          
+          double mid_val_x = val.x() + delta_t * v_intp_half[key].x();
+          double mid_val_y = val.y() + delta_t * v_intp_half[key].y();
+          
+          // if close to 0 using EPS, make 0, or greater than lx or ly, make lx
+          // or ly
+          if (abs(mid_val_x) < dbl_epsilon || abs(mid_val_x - lx_) < dbl_epsilon)
+            mid_val_x = (abs(mid_val_x) < dbl_epsilon) ? 0 : lx_;
+          if (abs(mid_val_y) < dbl_epsilon || abs(mid_val_y - ly_) < dbl_epsilon)
+            mid_val_y = (abs(mid_val_y) < dbl_epsilon) ? 0 : ly_;
+          
+          Point mid_val(mid_val_x, mid_val_y);
           mid.insert_or_assign(key, mid_val);
 
           // Do not accept the integration step if the maximum squared
