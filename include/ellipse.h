53078dafbbf69d7f820177aff316c4b0d55341b7
nihalzp <81457724+nihalzp@users.noreply.github.com>
2023-12-30 11:44:51 +0800

    Fix typo in ellipse.h

--- include/ellipse.h
+++ include/ellipse.h
@@ -10,7 +10,7 @@ struct Ellipse {
   double theta;
 
   // To avoid calculating trigonometric functions, we do not directly store
-  // the angle theta between the x-axis and the semimajor axis. Instead we
+  // the angle theta between the x-axis and the semimajor axis. Instead, we
   // store the cosine and sine of theta.
   double cos_theta;
   double sin_theta;
