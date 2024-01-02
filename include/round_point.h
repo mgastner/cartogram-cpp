d4f65dc971c0841cd1ac96dd0ffe5d5b4b31f457
nihalzp <81457724+nihalzp@users.noreply.github.com>
2024-01-02 16:00:43 +0800

    Remove redundant function xy_points_almost_equal

--- include/round_point.h
+++ include/round_point.h
@@ -2,12 +2,10 @@
 #define ROUND_POINT_H_
 
 #include "cgal_typedef.h"
-#include "xy_point.h"
 
 bool almost_equal(const double, const double);
 bool points_almost_equal(const Point, const Point);
 bool point_less_than(const Point, const Point);
-bool xy_points_almost_equal(const XYPoint, const XYPoint);
 double rounded_to_bicimal(
   const double,
   const unsigned int,
