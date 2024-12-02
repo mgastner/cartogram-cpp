3f0cd841054652363c042a28261845fa3a664f96
nihalzp <81457724+nihalzp@users.noreply.github.com>
2024-12-02 14:23:18 +0800

    Refactor read_geojson to remove dependency of csv

--- include/cartogram_info.hpp
+++ include/cartogram_info.hpp
@@ -10,9 +10,12 @@ private:
   std::map<std::string, std::string> gd_to_inset_;
   std::string id_header_;
   std::set<std::string> ids_in_visual_variables_file_;
+  std::vector<std::string> initial_id_order_;
   std::map<std::string, InsetState> inset_states_;
   bool is_world_map_;
   std::string map_name_;
+  std::map<std::string, std::map<std::string, std::string>> properties_map_;
+  std::vector<std::string> unique_properties_;
 
   // TODO: We assume that either all external rings are counterclockwise or
   //       all are clockwise. This dichotomy covers most geospatial boundary
@@ -22,8 +25,9 @@ private:
   nlohmann::json cgal_to_json(bool = false);
 
 public:
-  explicit CartogramInfo(bool, const std::string &);
+  explicit CartogramInfo(bool);
   [[nodiscard]] double cart_initial_total_target_area() const;
+  void construct_inset_state_from_geodivs(const nlohmann::json &);
   [[nodiscard]] double area() const;
   [[nodiscard]] bool is_world_map() const;
   void json_to_geojson(
@@ -35,11 +39,19 @@ public:
   void read_csv(const argparse::ArgumentParser &);
   void read_geojson(const std::string &, bool, std::string &);
   std::map<std::string, InsetState> &ref_to_inset_states();
+  void relocate_geodivs_based_on_inset_pos(
+    const std::map<std::string, std::map<std::string, std::string>> &);
   void replace_missing_and_zero_target_areas();
   std::string set_map_name(const std::string &);
+  void set_id_header(const std::string &);
   void shift_insets_to_target_position(bool = false);
+  void update_id_header_info(const std::string &);
   void write_csv(const std::string &csv_file_name);
-  void write_geojson(const std::string &, const std::string &, bool = false);
+  void write_geojson(
+    const std::string &,
+    const std::string &,
+    bool = false,
+    bool = false);
 };
 
-#endif // CARTOGRAM_INFO_HPP_
+#endif  // CARTOGRAM_INFO_HPP_
