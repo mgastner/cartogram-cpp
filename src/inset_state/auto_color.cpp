4de834fa579556cdb5942961cba4a27c3feae07e
adisidev <64905594+adisidev@users.noreply.github.com>
2025-03-28 14:59:44 +0800

    Assign light-grey to GeoDivs that failed coloring

--- src/inset_state/auto_color.cpp
+++ src/inset_state/auto_color.cpp
@@ -5,7 +5,8 @@
 void InsetState::auto_color()
 {
   // Colors are already provided
-  if (colors_.size() == n_geo_divs()) return;
+  if (colors_.size() == n_geo_divs())
+    return;
 
   // Some colors are already provided, use light grey for the rest
   if (colors_.size() > 0) {
@@ -21,14 +22,14 @@ void InsetState::auto_color()
   // Using default palette for now
   // TODO: Accept palette from user
   // From https://colorbrewer2.org/#type=qualitative&scheme=Pastel1&n=8
-  palette.emplace_back("#fbb4ae"); // red
-  palette.emplace_back("#b3cde3"); // blue
-  palette.emplace_back("#ccebc5"); // green
-  palette.emplace_back("#decbe4"); // purple
-  palette.emplace_back("#fed9a6"); // orange
-  palette.emplace_back("#ffffcc"); // yellow
-  palette.emplace_back("#e5d8bd"); // brown
-  palette.emplace_back("#fddaec"); // pink
+  palette.emplace_back("#fbb4ae");  // red
+  palette.emplace_back("#b3cde3");  // blue
+  palette.emplace_back("#ccebc5");  // green
+  palette.emplace_back("#decbe4");  // purple
+  palette.emplace_back("#fed9a6");  // orange
+  palette.emplace_back("#ffffcc");  // yellow
+  palette.emplace_back("#e5d8bd");  // brown
+  palette.emplace_back("#fddaec");  // pink
 
   // Create continuity graph based on vertical and horizontal scanlines
   create_contiguity_graph();
@@ -57,5 +58,20 @@ void InsetState::auto_color()
         break;
       }
     }
+  }
+
+  // Check if all GeoDivs are colored
+  if (colors_.size() != n_geo_divs()) {
+    std::cerr
+      << "WARNING: Unable to color all GeoDivs without adjacency collisions"
+      << std::endl;
+    std::cerr
+      << "Assigning #f2f2f2 (light grey) to uncolored GeoDivs"
+      << std::endl;
+      for (GeoDiv &gd : geo_divs_) {
+        if (!color_found(gd.id())) {
+          insert_color(gd.id(), Color("#f2f2f2"));
+        }
+      }
   }
 }
