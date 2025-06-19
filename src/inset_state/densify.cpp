0ce470729c7818260f3cf39e9f84b603b7ca2d77
nihalzp <81457724+nihalzp@users.noreply.github.com>
2025-06-19 16:05:32 +0800

    Optimize densification

--- src/inset_state/densify.cpp
+++ src/inset_state/densify.cpp
@@ -319,6 +319,7 @@ static std::vector<Point> densification_points_with_delaunay_t(
   const Delaunay &dt)
 {
   std::vector<Point> dens_points;
+  dens_points.reserve(8);
 
   // keep track of visited segments
   std::unordered_set<Segment> vis_seg;
@@ -340,7 +341,6 @@ static std::vector<Point> densification_points_with_delaunay_t(
   Segment segment(pt1, pt2);  // segment to be densified
 
   Line_face_circulator lfc = dt.line_walk(pt1, pt2);
-
   Line_face_circulator lfc_begin = lfc;  // we store the begining iterator
 
   // keeping track of states
@@ -354,7 +354,7 @@ static std::vector<Point> densification_points_with_delaunay_t(
           break;
         }
       } else if (fh == f1) {  // have not found a matching face yet; we start
-                              // with pt2 point
+                              // with pt1 point
         f1_first = true;
         found_a_face = true;
 
@@ -378,7 +378,7 @@ static std::vector<Point> densification_points_with_delaunay_t(
       for (const Segment &tri_seg : {s1, s2, s3}) {
         Segment tri_seg_rev(tri_seg.target(), tri_seg.source());
 
-        // if the segment is already visited, continue
+        // if the segment (undirected) is already visited, continue
         if (
           vis_seg.find(tri_seg) != vis_seg.end() ||
           vis_seg.find(tri_seg_rev) != vis_seg.end()) {
@@ -419,7 +419,7 @@ static std::vector<Point> densification_points_with_delaunay_t(
 
   // if densification points are in reverse order, reverse them
   if (!almost_equal(dens_points.front(), pt1)) {
-    reverse(dens_points.begin(), dens_points.end());
+    std::reverse(dens_points.begin(), dens_points.end());
   }
 
   // check validity of densification points: in case the first and last
@@ -433,6 +433,7 @@ static std::vector<Point> densification_points_with_delaunay_t(
 
   // keep densified points those are different from the first and last points
   std::vector<Point> dens_points_unique;
+  dens_points_unique.reserve(dens_points.size());
   dens_points_unique.push_back(dens_points.front());
   for (unsigned int i = 1; i < dens_points.size() - 1; ++i) {
     if (
@@ -451,26 +452,32 @@ void InsetState::densify_geo_divs_using_delaunay_t()
 {
   timer.start("Densification (using Delanuay Triangles)");
   // Empty set containing densified points from previous iteration
-  points_from_densification_.clear();
-  points_before_densification_.clear();
+  // points_from_densification_.clear();
+  // points_before_densification_.clear();
 
   std::cerr << "Densifying using Delaunay Triangulation" << std::endl;
   std::vector<GeoDiv> geodivs_dens;
+  geodivs_dens.reserve(geo_divs_.size());
+
   for (const auto &gd : geo_divs_) {
     GeoDiv gd_dens(gd.id());
+
     for (const auto &pwh : gd.polygons_with_holes()) {
       const auto &outer = pwh.outer_boundary();
       Polygon outer_dens;
+      outer_dens.reserve(outer.size() * 5);
 
       // Iterate over each point in the outer boundary of the polygon
-      for (unsigned int i = 0; i < outer.size(); ++i) {
+      const std::size_t outer_sz = outer.size();
+      for (std::size_t i = 0; i < outer_sz; ++i) {
 
         // The segment defined by points `a` and `b` is to be densified.
         // `b` should be the vertex of the boundary immediately after `a`,
         // unless `a` is the final vertex of the boundary, in which case `b`
         // should be the first vertex.
-        const auto a = outer[i];
-        const auto b = (i == outer.size() - 1) ? outer[0] : outer[i + 1];
+        const Point a = outer[i];
+        const Point b = (i + 1 == outer_sz) ? outer[0] : outer[i + 1];
+
         // Densify the segment
         const std::vector<Point> outer_pts_dens =
           densification_points_with_delaunay_t(a, b, proj_qd_.dt);
@@ -478,51 +485,62 @@ void InsetState::densify_geo_divs_using_delaunay_t()
         // Push all points. Omit the last point because it will be included
         // in the next iteration. Otherwise, we would have duplicated points
         // in the polygon.
-        for (unsigned int j = 0; j < (outer_pts_dens.size() - 1); ++j) {
-          outer_dens.push_back(outer_pts_dens[j]);
-        }
+        if (outer_pts_dens.size() > 1)
+          outer_dens.insert(
+            outer_dens.end(),
+            outer_pts_dens.begin(),
+            outer_pts_dens.end() - 1);
       }
 
       // Add new points to points_added_via_densification for plotting
-      std::unordered_set<Point> new_in_outer = new_points(outer, outer_dens);
-      points_from_densification_.insert(
-        new_in_outer.begin(),
-        new_in_outer.end());
-      points_before_densification_.insert(outer.begin(), outer.end());
+      // std::unordered_set<Point> new_in_outer = new_points(outer,
+      // outer_dens); points_from_densification_.insert(
+      //   new_in_outer.begin(),
+      //   new_in_outer.end());
+      // points_before_densification_.insert(outer.begin(), outer.end());
 
       std::vector<Polygon> holes_v_dens;
+      holes_v_dens.reserve(pwh.number_of_holes());
 
       // Iterate over each hole
       for (auto const &h : pwh.holes()) {
+
         Polygon hole_dens;
-        for (unsigned int j = 0; j < h.size(); ++j) {
+        hole_dens.reserve(h.size() * 2);
+
+        const std::size_t h_sz = h.size();
+        for (std::size_t j = 0; j < h_sz; ++j) {
 
           // `c` and `d` are determined in the same way as `a` and `b` above
           const Point c = h[j];
-          const Point d = (j == h.size() - 1) ? h[0] : h[j + 1];
+          const Point d = (j + 1 == h_sz) ? h[0] : h[j + 1];
+
           const std::vector<Point> hole_pts_dens =
             densification_points_with_delaunay_t(c, d, proj_qd_.dt);
-          for (unsigned int i = 0; i < (hole_pts_dens.size() - 1); ++i) {
-            hole_dens.push_back(hole_pts_dens[i]);
-          }
+
+          if (hole_pts_dens.size() > 1)
+            hole_dens.insert(
+              hole_dens.end(),
+              hole_pts_dens.begin(),
+              hole_pts_dens.end() - 1);
         }
 
         // Add new points to points_added_via_densification for plotting
-        std::unordered_set<Point> new_in_hole = new_points(h, hole_dens);
-        points_from_densification_.insert(
-          new_in_hole.begin(),
-          new_in_hole.end());
-        points_before_densification_.insert(h.begin(), h.end());
+        // std::unordered_set<Point> new_in_hole = new_points(h, hole_dens);
+        // points_from_densification_.insert(
+        //   new_in_hole.begin(),
+        //   new_in_hole.end());
+        // points_before_densification_.insert(h.begin(), h.end());
 
-        holes_v_dens.push_back(hole_dens);
+        holes_v_dens.emplace_back(std::move(hole_dens));
       }
       const Polygon_with_holes pwh_dens(
-        outer_dens,
+        std::move(outer_dens),
         holes_v_dens.begin(),
         holes_v_dens.end());
-      gd_dens.push_back(pwh_dens);
+      gd_dens.push_back(std::move(pwh_dens));
     }
-    geodivs_dens.push_back(gd_dens);
+    geodivs_dens.emplace_back(std::move(gd_dens));
   }
   geo_divs_ = std::move(geodivs_dens);
   is_simple(__func__);
