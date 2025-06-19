9d039fd2db769ed09a4f1388fc10003d952262d0
nihalzp <81457724+nihalzp@users.noreply.github.com>
2025-06-19 15:16:28 +0800

    Optimize simplification

--- src/inset_state/simplify_inset.cpp
+++ src/inset_state/simplify_inset.cpp
@@ -13,22 +13,21 @@ void InsetState::simplify(const unsigned int target_points_per_inset)
     std::cerr << "No need for simplification." << std::endl;
     return;
   }
+  const unsigned int n_rings = this->n_rings();
+  const unsigned long target_pts =
+    std::max(target_points_per_inset, min_points_per_ring * n_rings);
+  const double ratio =
+    static_cast<double>(target_pts) / static_cast<double>(n_pts_before);
   std::cerr << "Simplifying the inset. " << std::endl;
 
   // Store Polygons as a CT (Constrained Triangulation) object. Code inspired
   // by https://doc.cgal.org/latest/Polyline_simplification_2/index.html
-  std::unordered_map<int, Constraint_id> pgn_id_to_constraint_id;
-  int pgn_id = 0;
+  std::vector<Constraint_id> pgn_id_to_constraint_id(n_rings);
+  size_t pgn_id = 0;
   CT ct;
   for (const auto &gd : geo_divs_) {
     for (const auto &pwh : gd.polygons_with_holes()) {
       pgn_id_to_constraint_id[pgn_id++] =
-        // NOTE: This line causes a segmentation fault in
-        // cartogram
-        // ../../sample_data/world_by_country_since_2022/world_by_country_since_2022.geojson
-        // ../../sample_data/world_by_country_since_2022/world_population_2010.csv
-        // -QST in DEBUG mode
-        // TODO: Investigate why
         ct.insert_constraint(pwh.outer_boundary());
       for (const auto &h : pwh.holes()) {
         pgn_id_to_constraint_id[pgn_id++] = ct.insert_constraint(h);
@@ -37,46 +36,41 @@ void InsetState::simplify(const unsigned int target_points_per_inset)
   }
 
   // Simplify polygons
-  const unsigned long target_pts =
-    std::max(target_points_per_inset, min_points_per_ring * n_rings());
-  const double ratio =
-    static_cast<double>(target_pts) / static_cast<double>(n_pts_before);
   CGAL::Polyline_simplification_2::simplify(ct, Cost(), Stop(ratio));
 
   // Store simplified polygons
   pgn_id = 0;
   for (auto &gd : geo_divs_) {
     for (auto &pwh : gd.ref_to_polygons_with_holes()) {
+
       auto cit = pgn_id_to_constraint_id[pgn_id++];
+      auto beg = ct.vertices_in_constraint_begin(cit);
+      auto end = ct.vertices_in_constraint_end(cit);
 
       Polygon ext_ring;
-      for (auto it = ct.vertices_in_constraint_begin(cit);
-           it != ct.vertices_in_constraint_end(cit);
-           ++it) {
-        ext_ring.push_back(((*it)->point()));
-      }
-
-      // remove the last point which is identical to the first point to
-      // make it a simple polygon
-      ext_ring.erase(ext_ring.vertices_end() - 1);
+      ext_ring.reserve(std::distance(beg, end) - 1);
+      for (auto it = beg; std::next(it) != end; ++it)
+        ext_ring.push_back((*it)->point());
 
       std::vector<Polygon> int_ring_v;
+      int_ring_v.reserve(pwh.number_of_holes());
       for (auto h : pwh.holes()) {
         cit = pgn_id_to_constraint_id[pgn_id++];
-        Polygon int_ring;
-        for (auto it = ct.vertices_in_constraint_begin(cit);
-             it != ct.vertices_in_constraint_end(cit);
-             ++it) {
-          int_ring.push_back(((*it)->point()));
-        }
+        beg = ct.vertices_in_constraint_begin(cit);
+        end = ct.vertices_in_constraint_end(cit);
 
-        // remove the last point which is identical to the first point to
-        // make it a simple polygon
-        int_ring.erase(int_ring.vertices_end() - 1);
-        int_ring_v.push_back(int_ring);
+        Polygon int_ring;
+        int_ring.reserve(std::distance(beg, end) - 1);
+        for (auto it = beg; std::next(it) != end; ++it)
+          int_ring.push_back((*it)->point());
+
+        int_ring_v.emplace_back(std::move(int_ring));
       }
-      Polygon_with_holes pgnwh =
-        Polygon_with_holes(ext_ring, int_ring_v.begin(), int_ring_v.end());
+
+      Polygon_with_holes pgnwh(
+        std::move(ext_ring),
+        int_ring_v.begin(),
+        int_ring_v.end());
       pwh = std::move(pgnwh);
     }
   }
