35d38ad272f89f461299f4fbb0d327cbf6c70fbf
nihalzp <81457724+nihalzp@users.noreply.github.com>
2025-10-15 14:33:00 +0800

    Bump version to 25.10

--- src/misc/parse_arguments.cpp
+++ src/misc/parse_arguments.cpp
@@ -8,7 +8,7 @@ Arguments parse_arguments(const int argc, const char *argv[])
 
   // Create parser for arguments using argparse.
   // From https://github.com/p-ranav/argparse
-  argparse::ArgumentParser arguments("./cartogram", "25.9");
+  argparse::ArgumentParser arguments("./cartogram", "25.10");
 
   // Positional argument accepting geometry file (GeoJSON, JSON) as input
   arguments.add_argument("geometry_file")
